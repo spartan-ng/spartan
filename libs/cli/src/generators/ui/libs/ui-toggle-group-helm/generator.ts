a5912695a6f2732e97aa340b3418b0885eb66753
Leonidas <leonidas@spartan.ng>
2025-04-10 01:13:28 +0000

    chore: nightly release 2025-04-10 ⚡

---
+++ libs/cli/src/generators/ui/libs/ui-toggle-group-helm/generator.ts
@@ -0,0 +1,12 @@
+import { Tree } from '@nx/devkit';
+import hlmBaseGenerator from '../../../base/generator';
+import type { HlmBaseGeneratorSchema } from '../../../base/schema';
+
+export async function generator(tree: Tree, options: HlmBaseGeneratorSchema) {
+	return await hlmBaseGenerator(tree, {
+		...options,
+		primitiveName: 'togglegroup',
+		internalName: 'ui-toggle-group-helm',
+		publicName: 'ui-togglegroup-helm',
+	});
+}

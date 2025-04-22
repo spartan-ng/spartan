7f5f31aa31c64db9f05b8be5f645ab19ae163a58
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-04-22 22:28:46 +0100

    feat(collapsible): adding expanded input and output

--- libs/brain/collapsible/src/lib/brn-collapsible-trigger.directive.ts
+++ libs/brain/collapsible/src/lib/brn-collapsible-trigger.directive.ts
@@ -1,35 +1,27 @@
-import { Directive, computed, inject } from '@angular/core';
+import { Directive, inject } from '@angular/core';
 import { BrnCollapsibleComponent } from './brn-collapsible.component';
 
 @Directive({
 	selector: 'button[brnCollapsibleTrigger]',
 	standalone: true,
 	host: {
-		'[attr.data-state]': 'state()',
-		'[attr.disabled]': 'attrDisabled()',
-		'[attr.aria-expanded]': 'state() === "open"',
-		'[attr.aria-controls]': 'ariaControls()',
-		'(click)': 'toggleCollapsible()',
+		'[attr.data-state]': 'collapsible?.expanded() ? "open" : "closed"',
+		'[attr.disabled]': 'collapsible?.disabled() ? true : undefined',
+		'[attr.aria-expanded]': 'collapsible?.expanded()',
+		'[attr.aria-controls]': 'collapsible?.contentId()',
+		'(click)': 'toggle()',
 	},
 })
 export class BrnCollapsibleTriggerDirective {
-	private readonly _collapsible = inject(BrnCollapsibleComponent, { optional: true });
-
-	public readonly state = computed(() => this._collapsible?.state());
-
-	public readonly disabled = computed(() => this._collapsible?.disabled?.());
-
-	public readonly attrDisabled = computed(() => this._collapsible?.attrDisabled?.());
-
-	public readonly ariaControls = computed(() => this._collapsible?.contentId());
+	protected readonly collapsible = inject(BrnCollapsibleComponent, { optional: true });
 
 	constructor() {
-		if (!this._collapsible) {
+		if (!this.collapsible) {
 			throw Error('Collapsible trigger directive can only be used inside a brn-collapsible element.');
 		}
 	}
 
-	toggleCollapsible() {
-		this._collapsible?.toggle();
+	toggle(): void {
+		this.collapsible?.toggle();
 	}
 }

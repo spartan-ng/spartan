2f6d17c248d93d010af70264ad113c332004731f
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-05-20 10:38:38 +0200

    docs(sonner): update code snippets

--- apps/app/src/app/pages/(components)/components/(sonner)/sonner.preview.ts
+++ apps/app/src/app/pages/(components)/components/(sonner)/sonner.preview.ts
@@ -23,37 +23,38 @@ export class SonnerPreviewComponent {
 	}
 }
 
-export const defaultCode = `import { Component } from '@angular/core';
-import { toast } from 'ngx-sonner';
-import { HlmToasterComponent } from '@spartan-ng/ui-sonner-helm';
+export const defaultCode = `
+import { Component } from '@angular/core';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmToasterComponent } from '@spartan-ng/ui-sonner-helm';
+import { toast } from 'ngx-sonner';
 
 @Component({
 	selector: 'spartan-sonner-preview',
-	standalone: true,
 	imports: [HlmToasterComponent, HlmButtonDirective],
 	template: \`
-	  <hlm-toaster />
-	  <button hlmBtn (click)="showToast()">Show Toast</button>
+		<hlm-toaster />
+		<button hlmBtn (click)="showToast()">Show Toast</button>
 	\`,
 })
 export class SonnerPreviewComponent {
-  showToast() {
-    toast('Event has been created', {
-      description: 'Sunday, December 03, 2024 at 9:00 AM',
-      action: {
-        label: 'Undo',
-        onClick: () => console.log('Undo'),
-      }
-    })
-  }
-}`;
+	showToast() {
+		toast('Event has been created', {
+			description: 'Sunday, December 03, 2023 at 9:00 AM',
+			action: {
+				label: 'Undo',
+				onClick: () => console.log('Undo'),
+			},
+		});
+	}
+}
+`;
 
 export const defaultImports = `
-import { toast } from 'ngx-sonner';
 import { HlmToasterComponent } from '@spartan-ng/ui-sonner-helm';
+import { toast } from 'ngx-sonner';
 `;
 export const defaultSkeleton = `
 <hlm-toaster />
-<button (click)="toast('Hello world')">Show Toast</button>
+<button hlmBtn (click)="showToast()">Show Toast</button>
 `;

9f342cf2125cb8c7cdd3473e8961b58d0f520c5f
Drayke <officialdrayke@outlook.de>
2025-05-21 16:40:56 +0000

    fix(eslint): Disabled @angular-eslint/component-selector rule for button and input directive

--- libs/ui/alert-dialog/helm/src/lib/hlm-alert-dialog-action-button.directive.ts
+++ libs/ui/alert-dialog/helm/src/lib/hlm-alert-dialog-action-button.directive.ts
@@ -2,6 +2,7 @@ import { Directive } from '@angular/core';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Directive({
+	// eslint-disable-next-line @angular-eslint/component-selector
 	selector: 'button[hlmAlertDialogAction]',
 	standalone: true,
 	hostDirectives: [HlmButtonDirective],

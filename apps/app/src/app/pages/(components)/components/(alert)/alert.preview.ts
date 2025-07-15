649ec9dffe374b819814dbe83993b404084a7f24
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-07-15 13:19:04 +0200

    docs(alert): add additional alert examples

--- apps/app/src/app/pages/(components)/components/(alert)/alert.preview.ts
+++ apps/app/src/app/pages/(components)/components/(alert)/alert.preview.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideCircleCheck } from '@ng-icons/lucide';
+import { lucideCircleAlert, lucideCircleCheck, lucidePopcorn } from '@ng-icons/lucide';
 import {
 	HlmAlertDescriptionDirective,
 	HlmAlertDirective,
@@ -19,12 +19,30 @@ import { HlmIconDirective } from '@spartan-ng/helm/icon';
 		NgIcon,
 		HlmIconDirective,
 	],
-	providers: [provideIcons({ lucideCircleCheck })],
+	providers: [provideIcons({ lucideCircleCheck, lucidePopcorn, lucideCircleAlert })],
 	template: `
-		<div hlmAlert>
-			<ng-icon hlm hlmAlertIcon name="lucideCircleCheck" />
-			<h4 hlmAlertTitle>Success! Your changes have been saved</h4>
-			<p hlmAlertDescription>This is an alert with icon, title and description.</p>
+		<div class="grid w-full max-w-xl items-start gap-4">
+			<div hlmAlert>
+				<ng-icon hlm hlmAlertIcon name="lucideCircleCheck" />
+				<h4 hlmAlertTitle>Success! Your changes have been saved</h4>
+				<p hlmAlertDescription>This is an alert with icon, title and description.</p>
+			</div>
+			<div hlmAlert>
+				<ng-icon hlm hlmAlertIcon name="lucidePopcorn" />
+				<h4 hlmAlertTitle>This Alert has a title and an icon. No description.</h4>
+			</div>
+			<div hlmAlert variant="destructive">
+				<ng-icon hlm hlmAlertIcon name="lucideCircleAlert" />
+				<h4 hlmAlertTitle>Unable to process your payment.</h4>
+				<div hlmAlertDescription>
+					<p>Please verify your billing information and try again.</p>
+					<ul class="list-inside list-disc text-sm">
+						<li>Check your card details</li>
+						<li>Ensure sufficient funds</li>
+						<li>Verify billing address</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	`,
 })
@@ -40,7 +58,7 @@ import {
 `;
 
 export const defaultSkeleton = `
-<div hlmAlert>
+<div hlmAlert variant="default | destructive">
   <ng-icon hlm hlmAlertIcon name="lucideCircleCheck" />
   <h4 hlmAlertTitle>Success! Your changes have been saved</h4>
   <div hlmAlertDescription>This is an alert with icon, title and description.</div>

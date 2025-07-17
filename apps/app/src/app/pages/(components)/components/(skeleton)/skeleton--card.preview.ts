3c663a4d2904f61e1588225e47655f9a52b1bf0e
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-07-17 09:58:14 +0200

    docs(skeleton): add card example

---
+++ apps/app/src/app/pages/(components)/components/(skeleton)/skeleton--card.preview.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { HlmSkeletonComponent } from '@spartan-ng/helm/skeleton';
+
+@Component({
+	selector: 'spartan-skeleton-card',
+	imports: [HlmSkeletonComponent],
+	template: `
+		<div class="flex flex-col space-y-3">
+			<hlm-skeleton class="h-[125px] w-[250px] rounded-xl" />
+			<div class="space-y-2">
+				<hlm-skeleton class="h-4 w-[250px]" />
+				<hlm-skeleton class="h-4 w-[200px]" />
+			</div>
+		</div>
+	`,
+})
+export class SkeletonCardComponent {}

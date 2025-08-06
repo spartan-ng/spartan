cad663ace02a25698500d5d2c4642b50f563d0b8
Merlin Moos <merlin.moos@oldendorff.com>
2025-08-06 17:30:38 +0200

    feat(cli): eslint rules alignment #802

--- libs/cli/src/generators/base/lib/initialize-angular-library.ts
+++ libs/cli/src/generators/base/lib/initialize-angular-library.ts
@@ -1,15 +1,94 @@
 import { UnitTestRunner } from '@nx/angular/generators';
 import { type Tree, joinPathFragments, readNxJson } from '@nx/devkit';
+import * as path from 'path';
+import { ObjectLiteralExpression, Project, SyntaxKind } from 'ts-morph';
 import type { HlmBaseGeneratorSchema } from '../schema';
 
+function addRulesToEslintOverride(tree: Tree, libRoot: string, filePattern: string, newRules: Record<string, string>) {
+	const eslintPath = joinPathFragments(libRoot, 'eslint.config.mjs');
+	if (!tree.exists(eslintPath)) return;
+
+	const eslintFullPath = path.join(process.cwd(), eslintPath);
+
+	const project = new Project({
+		tsConfigFilePath: path.join(process.cwd(), 'tsconfig.base.json'),
+		skipAddingFilesFromTsConfig: true,
+	});
+
+	const sourceFile = project.createSourceFile(eslintFullPath, tree.read(eslintPath, 'utf-8')!, { overwrite: true });
+
+	const exportArray = sourceFile.getFirstDescendantByKind(SyntaxKind.ArrayLiteralExpression);
+	if (!exportArray) return;
+
+	const overrideObj = exportArray.getElements().find((el) => {
+		if (!el || !el.isKind(SyntaxKind.ObjectLiteralExpression)) return false;
+		const obj = el as ObjectLiteralExpression;
+		const filesProp = obj.getProperty('files');
+		return filesProp?.getText().includes(filePattern);
+	}) as ObjectLiteralExpression | undefined;
+
+	if (!overrideObj) return;
+
+	let rulesProp = overrideObj.getProperty('rules');
+	if (!rulesProp) {
+		overrideObj.addPropertyAssignment({ name: 'rules', initializer: '{}' });
+		rulesProp = overrideObj.getPropertyOrThrow('rules');
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const rulesObj = (rulesProp as any).getInitializerIfKindOrThrow(SyntaxKind.ObjectLiteralExpression);
+
+	for (const [ruleName, ruleValue] of Object.entries(newRules)) {
+		if (!rulesObj.getProperty(ruleName)) {
+			rulesObj.addPropertyAssignment({
+				name: ruleName,
+				initializer: ruleValue,
+			});
+		}
+	}
+
+	tree.write(eslintPath, sourceFile.getFullText());
+}
+
+/**
+ * Add TypeScript rules to ESLint flat config
+ */
+function addTsRules(tree: Tree, libRoot: string) {
+	addRulesToEslintOverride(tree, libRoot, '"**/*.ts"', {
+		'"@angular-eslint/no-input-rename"': '"off"',
+		'"@angular-eslint/directive-class-suffix"': '"off"',
+		'"@angular-eslint/component-class-suffix"': '"off"',
+		'"@typescript-eslint/naming-convention"': `[
+			"error",
+			{
+				"selector": "classProperty",
+				"modifiers": ["protected"],
+				"format": ["camelCase"],
+				"leadingUnderscore": "require"
+			}
+		]`,
+	});
+}
+
+/**
+ * Add HTML template rules to ESLint flat config
+ */
+function addHtmlRules(tree: Tree, libRoot: string) {
+	addRulesToEslintOverride(tree, libRoot, '"**/*.html"', {
+		'"@angular-eslint/template/interactive-supports-focus"': '"off"',
+		'"@angular-eslint/template/click-events-have-key-events"': '"off"',
+	});
+}
+
+function addRules(tree: Tree, libRoot: string) {
+	addTsRules(tree, libRoot);
+	addHtmlRules(tree, libRoot);
+}
+
 export async function initializeAngularLibrary(tree: Tree, options: HlmBaseGeneratorSchema) {
-	return await (
-		await import(
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			'@nx/angular/generators'
-		)
-	).libraryGenerator(tree, {
+	const { libraryGenerator } = await import('@nx/angular/generators');
+
+	const callback = await libraryGenerator(tree, {
 		...(readNxJson(tree).generators?.['@nx/angular:library'] || {}),
 		name: options.publicName,
 		skipFormat: true,
@@ -23,4 +102,8 @@ export async function initializeAngularLibrary(tree: Tree, options: HlmBaseGener
 		skipTests: true,
 		unitTestRunner: UnitTestRunner.None,
 	});
+
+	addRules(tree, joinPathFragments(options.directory, options.publicName));
+
+	return callback;
 }

105568965e3eda3d18ad8365ac4382a0dfdc03db
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-08-07 17:02:17 +0200

    feat(separator): add *SeparatorImports

--- libs/helm/separator/src/index.ts
+++ libs/helm/separator/src/index.ts
@@ -3,8 +3,10 @@ import { HlmSeparator } from './lib/hlm-separator';
 
 export * from './lib/hlm-separator';
 
+export const HlmSeparatorImports = [HlmSeparator] as const;
+
 @NgModule({
-	imports: [HlmSeparator],
-	exports: [HlmSeparator],
+	imports: [...HlmSeparatorImports],
+	exports: [...HlmSeparatorImports],
 })
 export class HlmSeparatorModule {}

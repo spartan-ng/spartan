a5f21bf3f5504d5cf72dfeaa7dc98a473549adff
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-08-07 17:02:17 +0200

    feat(separator): add *SeparatorImports

--- libs/brain/separator/src/index.ts
+++ libs/brain/separator/src/index.ts
@@ -3,8 +3,10 @@ import { BrnSeparator } from './lib/brn-separator';
 
 export * from './lib/brn-separator';
 
+export const BrnSeparatorImports = [BrnSeparator] as const;
+
 @NgModule({
-	imports: [BrnSeparator],
-	exports: [BrnSeparator],
+	imports: [...BrnSeparatorImports],
+	exports: [...BrnSeparatorImports],
 })
 export class BrnSeparatorModule {}

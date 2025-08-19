cfc31b5fa9c73fffbd67d270f88b4d4063352ea4
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-08-19 09:54:56 +0200

    feat(dialog): update styles and sort classes

--- libs/helm/dialog/src/lib/hlm-dialog-close.ts
+++ libs/helm/dialog/src/lib/hlm-dialog-close.ts
@@ -13,7 +13,7 @@ export class HlmDialogClose {
 
 	protected readonly _computedClass = computed(() =>
 		hlm(
-			'absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground',
+			'ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground rounded-xs focus:outline-hidden absolute right-4 top-4 opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none [&_ng-icon]:pointer-events-none [&_ng-icon]:shrink-0',
 			this.userClass(),
 		),
 	);

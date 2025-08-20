429fe26ce705cd067a0fb6ab10fed0ace410c247
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-08-20 12:06:07 +0200

    fix(dialog): use duration-200 for state open and closed

--- libs/helm/dialog/src/lib/hlm-dialog-content.ts
+++ libs/helm/dialog/src/lib/hlm-dialog-content.ts
@@ -43,7 +43,7 @@ export class HlmDialogContent {
 	public readonly userClass = input<ClassValue>('', { alias: 'class' });
 	protected readonly _computedClass = computed(() =>
 		hlm(
-			'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[2%] data-[state=open]:slide-in-from-top-[2%] relative mx-auto grid w-full max-w-[calc(100%-2rem)] gap-4 rounded-lg border p-6 shadow-lg [animation-duration:200] sm:max-w-lg',
+			'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[2%] data-[state=open]:slide-in-from-top-[2%] relative mx-auto grid w-full max-w-[calc(100%-2rem)] gap-4 rounded-lg border p-6 shadow-lg data-[state=closed]:duration-200 data-[state=open]:duration-200 sm:max-w-lg',
 			this.userClass(),
 			this._dynamicComponentClass,
 		),

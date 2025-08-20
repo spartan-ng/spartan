16cc4c1c79d9c9716a017984c580143d6054e007
Sammy Mohamed <sammymohamed1223@gmail.com>
2025-08-19 22:28:40 -0400

    fix(select): remove tailwindcss class from brn

--- libs/brain/select/src/lib/brn-select.ts
+++ libs/brain/select/src/lib/brn-select.ts
@@ -62,7 +62,7 @@ let nextId = 0;
 	],
 	template: `
 		@if (!_selectLabel() && placeholder()) {
-			<label class="hidden" [attr.id]="labelId()">{{ placeholder() }}</label>
+			<label style="display: none;" [attr.id]="labelId()">{{ placeholder() }}</label>
 		} @else {
 			<ng-content select="label[hlmLabel],label[brnLabel]" />
 		}

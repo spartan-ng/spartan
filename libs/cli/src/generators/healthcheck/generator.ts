3e878f1baf4b9e8b8a7079a7633a15b7763e1b27
moosme <46927822+MerlinMoos@users.noreply.github.com>
2025-08-24 22:29:00 +0200

    feat(date-picker): add date range picker #606 (#823)
    * feat(date-picker): add date range picker #606
    
    * feat(date-picker): finalize date range picker #606
    
    * feat(date-picker): add utc date adapter #606
    
    * feat(date-picker): add health checks #606
    
    * feat(date-picker): code suggestions #606
    
    * feat(date-picker): restore selected month #606
    
    ---------
    
    Co-authored-by: Merlin Moos <merlin.moos@oldendorff.com>

--- libs/cli/src/generators/healthcheck/generator.ts
+++ libs/cli/src/generators/healthcheck/generator.ts
@@ -5,6 +5,7 @@ import { brainRadioHealthcheck } from './healthchecks/brn-radio';
 import { brainToggleHealthcheck } from './healthchecks/brn-toggle-group';
 import { coreImportsHealthcheck } from './healthchecks/core-imports';
 import { helmImportsHealthcheck } from './healthchecks/helm-imports';
+import { datePickerHealthcheck } from './healthchecks/hlm-date-picker';
 import { helmIconHealthcheck } from './healthchecks/hlm-icon';
 import { progressHealthcheck } from './healthchecks/hlm-progress';
 import { scrollAreaHealthcheck } from './healthchecks/hlm-scroll-area';
@@ -30,6 +31,7 @@ export async function healthcheckGenerator(tree: Tree, options: HealthcheckGener
 		brainToggleHealthcheck,
 		helmImportsHealthcheck,
 		namingConventionHealthcheck,
+		datePickerHealthcheck,
 		progressHealthcheck,
 	];
 

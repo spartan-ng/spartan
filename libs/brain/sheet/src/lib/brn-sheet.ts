c4e03a5622410b008e6cbdde76c6069e06182ab3
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-09-01 19:11:21 +0200

    feat: dialog polishing (#836)
    * feat(dialog): update styles and sort classes
    
    # Conflicts:
    #	libs/helm/dialog/src/lib/hlm-dialog-close.ts
    #	libs/helm/dialog/src/lib/hlm-dialog-content.ts
    
    * fix(dialog): control dialog close button height w/ flex
    
    * button size was 16x24px w/ an icon of 16x16px
    * using flex changes the sizes of the button to 16x16px
    
    * feat(dialog): add --radiux-xs styles used by shadcn/ui
    
    * fix(dialog): use outline-none for close button again
    
    * docs(dialog): update example headings, form field and preview
    
    # Conflicts:
    #	apps/app/src/app/pages/(components)/components/(dialog)/dialog.preview.ts
    
    * docs(dialog): update bottom nav
    
    * feat(dialog): replace mutable state using computed + signal w/ linkedSignal
    
    * fix(dialog): remove [&_ng-icon]:pointer-events-none to fix e2e tests
    
    * fix(dialog): use duration-200 for state open and closed
    
    * fix(dialog): apply same styles like hlm-alert-dialog-content
    
    * fix(dialog): center dialog on mobile using mx-auto
    
    * justify-self-center is not applied in firefox/safari - parent container needs to be display grid

--- libs/brain/sheet/src/lib/brn-sheet.ts
+++ libs/brain/sheet/src/lib/brn-sheet.ts
@@ -35,16 +35,16 @@ export class BrnSheet extends BrnDialog {
 			const side = this.sideState();
 			untracked(() => {
 				if (side === 'top') {
-					this.mutablePositionStrategy().set(this.positionBuilder.global().top());
+					this.mutablePositionStrategy.set(this.positionBuilder.global().top());
 				}
 				if (side === 'bottom') {
-					this.mutablePositionStrategy().set(this.positionBuilder.global().bottom());
+					this.mutablePositionStrategy.set(this.positionBuilder.global().bottom());
 				}
 				if (side === 'left') {
-					this.mutablePositionStrategy().set(this.positionBuilder.global().left());
+					this.mutablePositionStrategy.set(this.positionBuilder.global().left());
 				}
 				if (side === 'right') {
-					this.mutablePositionStrategy().set(this.positionBuilder.global().right());
+					this.mutablePositionStrategy.set(this.positionBuilder.global().right());
 				}
 			});
 		});
--- libs/helm/dialog/src/lib/hlm-dialog-content.ts
+++ libs/helm/dialog/src/lib/hlm-dialog-content.ts
@@ -43,7 +43,7 @@ export class HlmDialogContent {
 	public readonly userClass = input<ClassValue>('', { alias: 'class' });
 	protected readonly _computedClass = computed(() =>
 		hlm(
-			'border-border bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[2%] data-[state=open]:slide-in-from-top-[2%] relative grid w-full max-w-lg gap-4 border p-6 shadow-lg [animation-duration:200] sm:rounded-lg md:w-full',
+			'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 relative z-50 mx-auto grid w-full max-w-[calc(100%-2rem)] gap-4 rounded-lg border p-6 shadow-lg data-[state=closed]:duration-200 data-[state=open]:duration-200 sm:mx-0 sm:max-w-lg',
 			this.userClass(),
 			this._dynamicComponentClass,
 		),

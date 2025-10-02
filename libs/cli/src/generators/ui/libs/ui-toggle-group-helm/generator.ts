04a1568369051e14120f777999a34f2dd463e7fd
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-10-02 18:13:01 +0200

    build: fix linting type errors

--- libs/cli/src/generators/ui/libs/ui-toggle-group-helm/generator.ts
+++ libs/cli/src/generators/ui/libs/ui-toggle-group-helm/generator.ts
@@ -1,4 +1,4 @@
-import { Tree } from '@nx/devkit';
+import type { Tree } from '@nx/devkit';
 import hlmBaseGenerator from '../../../base/generator';
 import type { HlmBaseGeneratorSchema } from '../../../base/schema';
 

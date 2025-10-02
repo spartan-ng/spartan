d6e8999bfd449bc66272fadbc4c9f4cf505cfbdf
Leonidas <leonidas@spartan.ng>
2025-10-02 01:09:40 +0000

    chore: nightly release 2025-10-02 ⚡

--- libs/cli/src/generators/ui/libs/ui-dialog-helm/generator.ts
+++ libs/cli/src/generators/ui/libs/ui-dialog-helm/generator.ts
@@ -1,4 +1,4 @@
-import type { Tree } from '@nx/devkit';
+import { Tree } from '@nx/devkit';
 import hlmBaseGenerator from '../../../base/generator';
 import type { HlmBaseGeneratorSchema } from '../../../base/schema';
 

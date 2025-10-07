342e088d5dce8abd32ad499a6e90706f5c428afe
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-10-07 11:08:16 +0200

    docs(data-table): remove pagination from preview, limit data to 5 items

--- apps/app/src/app/pages/(components)/components/(data-table)/data-table.preview.ts
+++ apps/app/src/app/pages/(components)/components/(data-table)/data-table.preview.ts
@@ -21,7 +21,6 @@ import {
 	getFilteredRowModel,
 	getPaginationRowModel,
 	getSortedRowModel,
-	type PaginationState,
 	type SortingState,
 } from '@tanstack/angular-table';
 import { ActionDropdown } from './action-dropdown';
@@ -122,49 +121,24 @@ export type Payment = {
 			}
 		</div>
 
-		<div class="mt-4 flex flex-col justify-between sm:flex-row sm:items-center">
+		<div class="flex flex-col justify-between py-4 sm:flex-row sm:items-center">
 			@if (_table.getRowCount() > 0) {
-				<span class="${hlmMuted} text-sm">
+				<div class="${hlmMuted}">
 					{{ _table.getSelectedRowModel().rows.length }} of {{ _table.getRowCount() }} row(s) selected
-				</span>
-				<div class="mt-2 flex sm:mt-0">
-					<brn-select
-						class="inline-block"
-						[ngModel]="_table.getState().pagination.pageSize"
-						(ngModelChange)="_table.setPageSize($event); _table.resetPageIndex()"
+				</div>
+				<div class="mt-2 flex space-x-2 sm:mt-0">
+					<button
+						size="sm"
+						variant="outline"
+						hlmBtn
+						[disabled]="!_table.getCanPreviousPage()"
+						(click)="_table.previousPage()"
 					>
-						<hlm-select-trigger class="w-15 mr-1 inline-flex h-9">
-							<hlm-select-value />
-						</hlm-select-trigger>
-						<hlm-select-content>
-							@for (size of _availablePageSizes; track size) {
-								<hlm-option [value]="size">
-									{{ size === 10000 ? 'All' : size }}
-								</hlm-option>
-							}
-						</hlm-select-content>
-					</brn-select>
-
-					<div class="flex space-x-1">
-						<button
-							size="sm"
-							variant="outline"
-							hlmBtn
-							[disabled]="!_table.getCanPreviousPage()"
-							(click)="_table.previousPage()"
-						>
-							Previous
-						</button>
-						<button
-							size="sm"
-							variant="outline"
-							hlmBtn
-							[disabled]="!_table.getCanNextPage()"
-							(click)="_table.nextPage()"
-						>
-							Next
-						</button>
-					</div>
+						Previous
+					</button>
+					<button size="sm" variant="outline" hlmBtn [disabled]="!_table.getCanNextPage()" (click)="_table.nextPage()">
+						Next
+					</button>
 				</div>
 			} @else {
 				<div class="flex h-full w-full items-center justify-center">
@@ -175,15 +149,12 @@ export type Payment = {
 	`,
 })
 export class DataTablePreview {
-	protected readonly _availablePageSizes = [5, 10, 20, 10000];
-
 	protected _filterChanged(event: Event) {
 		this._table.getColumn('email')?.setFilterValue((event.target as HTMLInputElement).value);
 	}
 
 	protected readonly _columns: ColumnDef<Payment>[] = [
 		{
-			accessorKey: 'select',
 			id: 'select',
 			header: () => flexRenderComponent(TableHeadSelection),
 			cell: () => flexRenderComponent(TableRowSelection),
@@ -194,13 +165,14 @@ export class DataTablePreview {
 			accessorKey: 'status',
 			id: 'status',
 			header: 'Status',
-			cell: (info) => `<span class="capitalize">${info.getValue<string>()}</span>`,
 			enableSorting: false,
+			cell: (info) => `<span class="capitalize">${info.getValue<string>()}</span>`,
 		},
 		{
 			accessorKey: 'email',
 			id: 'email',
 			header: () => flexRenderComponent(TableHeadSortButton, { inputs: { header: '' } }),
+			cell: (info) => `<div class="lowercase">${info.getValue<string>()}</div>`,
 		},
 		{
 			accessorKey: 'amount',
@@ -218,7 +190,7 @@ export class DataTablePreview {
 			},
 		},
 		{
-			id: 'action',
+			id: 'actions',
 			enableHiding: false,
 			cell: () => flexRenderComponent(ActionDropdown),
 		},
@@ -226,36 +198,23 @@ export class DataTablePreview {
 
 	private readonly _columnFilters = signal<ColumnFiltersState>([]);
 	private readonly _sorting = signal<SortingState>([]);
-	private readonly _pagination = signal<PaginationState>({
-		pageSize: 5,
-		pageIndex: 0,
-	});
 
 	protected readonly _table = createAngularTable<Payment>(() => ({
 		data: PAYMENT_DATA,
 		columns: this._columns,
-		state: {
-			columnFilters: this._columnFilters(),
-			sorting: this._sorting(),
-			pagination: this._pagination(),
+		onSortingChange: (updater) => {
+			updater instanceof Function ? this._sorting.update(updater) : this._sorting.set(updater);
 		},
 		onColumnFiltersChange: (updater) => {
 			updater instanceof Function ? this._columnFilters.update(updater) : this._columnFilters.set(updater);
 		},
-		onSortingChange: (updater) => {
-			updater instanceof Function ? this._sorting.update(updater) : this._sorting.set(updater);
-		},
-		onPaginationChange: (updater) => {
-			updater instanceof Function ? this._pagination.update(updater) : this._pagination.set(updater);
-		},
 		getCoreRowModel: getCoreRowModel(),
-		getFilteredRowModel: getFilteredRowModel(),
-		getSortedRowModel: getSortedRowModel(),
 		getPaginationRowModel: getPaginationRowModel(),
-		initialState: {
-			pagination: {
-				pageSize: 5,
-			},
+		getSortedRowModel: getSortedRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		state: {
+			columnFilters: this._columnFilters(),
+			sorting: this._sorting(),
 		},
 	}));
 	protected readonly _hidableColumns = this._table.getAllColumns().filter((column) => column.getCanHide());
@@ -297,95 +256,5 @@ const PAYMENT_DATA: Payment[] = [
 		amount: 721,
 		status: 'failed',
 		email: 'carmella@hotmail.com',
-	},
-	{
-		id: 'p0r8sd2f',
-		amount: 123,
-		status: 'failed',
-		email: 'john.doe@example.com',
-	},
-	{
-		id: '8uyv3n1x',
-		amount: 589,
-		status: 'processing',
-		email: 'emma.smith@gmail.com',
-	},
-	{
-		id: '2zqo6ptr',
-		amount: 456,
-		status: 'success',
-		email: 'jackson78@hotmail.com',
-	},
-	{
-		id: 'l7we9a3m',
-		amount: 632,
-		status: 'success',
-		email: 'grace_22@yahoo.com',
-	},
-	{
-		id: 'o9p2v3qk',
-		amount: 987,
-		status: 'failed',
-		email: 'robert.adams@gmail.com',
-	},
-	{
-		id: 'q1o8r7mz',
-		amount: 321,
-		status: 'processing',
-		email: 'alexander34@gmail.com',
-	},
-	{
-		id: 'i5n3s0tv',
-		amount: 555,
-		status: 'failed',
-		email: 'olivia_morris@hotmail.com',
-	},
-	{
-		id: '3xr7s2nl',
-		amount: 789,
-		status: 'success',
-		email: 'michael_cole@yahoo.com',
-	},
-	{
-		id: 'u9v2p1qy',
-		amount: 234,
-		status: 'success',
-		email: 'lily.jones@gmail.com',
-	},
-	{
-		id: 'b4q0e1cp',
-		amount: 876,
-		status: 'failed',
-		email: 'ryan_14@hotmail.com',
-	},
-	{
-		id: 's1z8m7op',
-		amount: 456,
-		status: 'success',
-		email: 'sophia.green@gmail.com',
-	},
-	{
-		id: 'n5a3v0lt',
-		amount: 987,
-		status: 'failed',
-		email: 'david.miller@yahoo.com',
-	},
-	{
-		id: '2qr7v9sm',
-		amount: 654,
-		status: 'processing',
-		email: 'emma_jones@hotmail.com',
-	},
-	{
-		id: 'y9b2h8qq',
-		amount: 789,
-		status: 'success',
-		email: 'jacob_89@gmail.com',
-	},
-	{
-		id: 'c4a0r1xp',
-		amount: 123,
-		status: 'failed',
-		email: 'samantha.richards@yahoo.com',
 	},
 ];

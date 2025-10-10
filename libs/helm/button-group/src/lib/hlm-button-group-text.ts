06f68338a76f42f79875d443e0e1bbb68b312f1b
dongphuong0905 <phuongtransmile99@gmail.com>
2025-10-10 16:38:18 +0700

    feat(button-group): update template

--- libs/helm/button-group/src/lib/hlm-button-group-text.ts
+++ libs/helm/button-group/src/lib/hlm-button-group-text.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from '@angular/core';
+import { computed, Directive, input } from '@angular/core';
 import { hlm } from '@spartan-ng/helm/utils';
 import { ClassValue } from 'clsx';
 
@@ -11,9 +11,10 @@ import { ClassValue } from 'clsx';
 export class HlmButtonGroupText {
 	public readonly userClass = input<ClassValue>('', { alias: 'class' });
 
-	protected readonly _computedClass = () =>
+	protected readonly _computedClass = computed(() =>
 		hlm(
 			"bg-muted shadow-xs flex items-center gap-2 rounded-md border px-4 text-sm font-medium [&_ng-icon:not([class*='size-'])]:size-4 [&_ng-icon]:pointer-events-none",
 			this.userClass(),
-		);
+		),
+	);
 }

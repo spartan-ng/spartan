8186ce1d6b9d3efde4ab2250e9fe2e0de495162f
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-10-13 10:04:28 +0200

    feat(input): remove HlmInputError, use Form Field HlmError
    # Conflicts:
    #	libs/cli/src/generators/ui/supported-ui-libraries.json

--- libs/helm/input/src/index.ts
+++ libs/helm/input/src/index.ts
@@ -1,7 +1,5 @@
 import { HlmInput } from './lib/hlm-input';
-import { HlmInputError } from './lib/hlm-input-error';
 
 export * from './lib/hlm-input';
-export * from './lib/hlm-input-error';
 
-export const HlmInputImports = [HlmInput, HlmInputError] as const;
+export const HlmInputImports = [HlmInput] as const;

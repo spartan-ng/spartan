80ebbc6f616bdc64a3280203e74c2f7860dbcfbc
robingotz <tug29225@temple.edu>
2025-10-14 12:19:50 +0200

    feat(accordion): enhance accordion content and fix item change events
    move to width/height signals for dynamic content sizing instead of grid.
    move contents inert attribute to host element
    move content style handlin to host element
    add state-based outputs to item for better reactivity and synced docs
    adjusted examples and stories to use hlm-accordion and hlm-accordion-item instead of divs
    
    closes #927, #884

--- libs/brain/accordion/src/lib/brn-accordion-content.ts
+++ libs/brain/accordion/src/lib/brn-accordion-content.ts
@@ -1,11 +1,16 @@
+import { isPlatformServer } from '@angular/common';
 import {
 	ChangeDetectionStrategy,
 	Component,
+	ElementRef,
+	OnInit,
+	PLATFORM_ID,
 	ViewEncapsulation,
 	computed,
 	inject,
 	input,
 	linkedSignal,
+	signal,
 } from '@angular/core';
 import type { CustomElementClassSettable } from '@spartan-ng/brain/core';
 import type { ClassValue } from 'clsx';
@@ -18,23 +23,30 @@ import { BrnAccordionItem } from './brn-accordion';
 		'[attr.aria-labelledby]': 'ariaLabeledBy',
 		role: 'region',
 		'[id]': 'id',
+		'[style.--brn-accordion-content-width.px]': '_width()',
+		'[style.--brn-accordion-content-height.px]': '_height()',
+		'[attr.inert]': '_addInert() ? true : undefined',
+		'[attr.style]': '_mutableContentStyle() ?? "opacity: 0;"',
 	},
 	template: `
-		<div [attr.inert]="_addInert()" style="overflow: hidden">
-			<p [class]="_contentClass()">
-				<ng-content />
-			</p>
-		</div>
+		<p [class]="_mutableContentClass()">
+			<ng-content />
+		</p>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	encapsulation: ViewEncapsulation.None,
 })
-export class BrnAccordionContent implements CustomElementClassSettable {
+export class BrnAccordionContent implements OnInit, CustomElementClassSettable {
 	private readonly _item = inject(BrnAccordionItem);
+	private readonly _elementRef = inject(ElementRef);
+	private readonly _platformId = inject(PLATFORM_ID);
 
 	public readonly state = this._item.state;
 	public readonly id = `brn-accordion-content-${this._item.id}`;
 	public readonly ariaLabeledBy = `brn-accordion-trigger-${this._item.id}`;
+	protected readonly _width = signal<number | null>(null);
+	protected readonly _height = signal<number | null>(null);
+	protected readonly _dimensionsInitiated = signal(false);
 
 	protected readonly _addInert = computed(() => (this.state() === 'closed' ? true : undefined));
 	/**
@@ -42,7 +54,15 @@ export class BrnAccordionContent implements CustomElementClassSettable {
 	 */
 	public readonly contentClass = input<ClassValue>('');
 
-	protected readonly _contentClass = linkedSignal(() => this.contentClass());
+	protected readonly _mutableContentClass = linkedSignal(() => this.contentClass());
+
+	/**
+	 * The style to be applied to the content element.
+	 */
+	public readonly contentStyle = input<string>('overflow: hidden;');
+	protected readonly _mutableContentStyle = linkedSignal(() =>
+		this._dimensionsInitiated() ? this.contentStyle() : undefined,
+	);
 
 	constructor() {
 		if (!this._item) {
@@ -50,7 +70,18 @@ export class BrnAccordionContent implements CustomElementClassSettable {
 		}
 	}
 
+	ngOnInit() {
+		if (isPlatformServer(this._platformId)) {
+			return;
+		}
+		const content = this._elementRef.nativeElement.firstChild as HTMLElement | null;
+		if (!content) return;
+		const { width, height } = content.getBoundingClientRect();
+		this._width.set(width);
+		this._height.set(height);
+		this._dimensionsInitiated.set(true);
+	}
 	public setClassToCustomElement(classes: ClassValue) {
-		this._contentClass.set(classes);
+		this._mutableContentClass.set(classes);
 	}
 }

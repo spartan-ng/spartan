5382fd049c206e8e6dd5bc3b67329866620f01b7
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-10-14 22:49:51 +0200

    docs: add new badge to navigation (#928)
    * docs: add new badge to navigation
    
    * refactor disabled to input signal
    * add ChangeDetectionStrategy.OnPush
    
    * docs: add new to button group

--- apps/app/src/app/shared/layout/side-nav/side-nav-coming-new.ts
+++ apps/app/src/app/shared/layout/side-nav/side-nav-coming-new.ts
@@ -1,13 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-	selector: 'spartan-side-nav-cs',
+	selector: 'spartan-side-nav-new',
 	host: {
 		class: 'inline-block',
 	},
 	template: `
-		<span class="text-primary-foreground bg-primary rounded-lg px-1 py-0.5 text-[0.5rem] font-medium">soon</span>
+		<span class="rounded-lg bg-[#DD0031] px-1 py-0.5 text-[0.5rem] font-medium text-white">new</span>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideNavCommingSoon {}
+export class SideNavNew {}

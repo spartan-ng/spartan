9eeafe58e394879d2e06d2767b679ead84786a75
Merlin Moos <merlin.moos@hotmail.de>
2025-10-18 00:03:27 +0200

    feat: add github stars

--- apps/app/src/server/trpc/routers/index.ts
+++ apps/app/src/server/trpc/routers/index.ts
@@ -1,8 +1,10 @@
+import { githubRouter } from '@spartan-ng/app/server/trpc/routers/github';
 import { router } from '../trpc';
 import { noteRouter } from './notes';
 
 export const appRouter = router({
 	note: noteRouter,
+	github: githubRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;

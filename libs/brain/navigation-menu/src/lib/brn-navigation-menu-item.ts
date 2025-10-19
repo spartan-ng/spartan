ec05908d7902c3912897d595c2b7ca1a45d52c65
Toni Škobić <toni.skobic@gmail.com>
2025-10-19 18:58:12 +0200

    fix(navigation-menu): fix navigation mouse out behaviour

--- libs/brain/navigation-menu/src/lib/brn-navigation-menu-item.ts
+++ libs/brain/navigation-menu/src/lib/brn-navigation-menu-item.ts
@@ -1,4 +1,4 @@
-import { computed, Directive, input, signal, TemplateRef } from '@angular/core';
+import { computed, Directive, inject, input, signal, TemplateRef } from '@angular/core';
 import { BrnNavigationMenuContentService } from './brn-navigation-menu-content.service';
 import { provideBrnNavigationMenuItem } from './brn-navigation-menu-item.token';
 import { injectBrnNavigationMenu } from './brn-navigation-menu.token';
@@ -14,6 +14,7 @@ export class BrnNavigationMenuItem {
 	private static _id = 0;
 
 	private readonly _navigationMenu = injectBrnNavigationMenu();
+	private readonly _contentService = inject(BrnNavigationMenuContentService);
 
 	/** The id of the navigation menu item */
 	public readonly id = input<string>(`brn-navigation-menu-item-${++BrnNavigationMenuItem._id}`);
@@ -25,4 +26,6 @@ export class BrnNavigationMenuItem {
 	public readonly state = computed(() => (this.isActive() ? 'open' : 'closed'));
 
 	public readonly contentTemplate = signal<TemplateRef<unknown> | null>(null);
+
+	public readonly contentHovered$ = this._contentService.hovered$;
 }

a84535edd8f93e713a75da79009475ed9a45d086
moosme <46927822+MerlinMoos@users.noreply.github.com>
2025-10-23 09:41:03 +0200

    feat(dropdown-menu): add side to brn menu trigger (#952)
    * feat(dropdown-menu): add side to brn menu trigger
    
    * feat(dropdown-menu): update snapshot
    
    ---------
    
    Co-authored-by: Merlin <merlin.moos@oldendorff.com>

--- libs/brain/menu/src/lib/brn-menu-align.ts
+++ libs/brain/menu/src/lib/brn-menu-align.ts
@@ -1,17 +1,29 @@
 import type { ConnectedPosition } from '@angular/cdk/overlay';
 
-export type BrnMenuAlign = 'start' | 'center' | 'end' | undefined;
-export const getBrnMenuAlign = (align: Exclude<BrnMenuAlign, undefined>): ConnectedPosition[] => [
-	{
-		originX: align,
-		originY: 'bottom',
-		overlayX: align,
-		overlayY: 'top',
-	},
-	{
-		originX: align,
-		originY: 'top',
-		overlayX: align,
-		overlayY: 'bottom',
-	},
-];
+export type BrnMenuAlign = 'start' | 'center' | 'end';
+export type BrnMenuSide = 'top' | 'bottom' | 'left' | 'right';
+
+export const getBrnMenuPosition = (align: BrnMenuAlign, side: BrnMenuSide): ConnectedPosition[] => {
+	const verticalAlign = align === 'start' ? 'top' : align === 'end' ? 'bottom' : 'center';
+
+	const createPositions = (
+		originX: 'start' | 'center' | 'end',
+		originY: 'top' | 'center' | 'bottom',
+		overlayX: 'start' | 'center' | 'end',
+		overlayY: 'top' | 'center' | 'bottom',
+	): ConnectedPosition[] => [
+		{ originX, originY, overlayX, overlayY },
+		{ originX: overlayX, originY: overlayY, overlayX: originX, overlayY: originY },
+	];
+
+	switch (side) {
+		case 'top':
+			return createPositions(align, 'top', align, 'bottom');
+		case 'bottom':
+			return createPositions(align, 'bottom', align, 'top');
+		case 'left':
+			return createPositions('start', verticalAlign, 'end', verticalAlign);
+		case 'right':
+			return createPositions('end', verticalAlign, 'start', verticalAlign);
+	}
+};

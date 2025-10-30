878beb9de36f305dfff8bb5da440541efbbc62f7
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-10-30 10:38:46 +0100

    docs(collapsible): use hlm collapsible in preview

--- apps/app/src/app/pages/(components)/components/(collapsible)/collapsible.preview.ts
+++ apps/app/src/app/pages/(components)/components/(collapsible)/collapsible.preview.ts
@@ -1,41 +1,39 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideChevronsUpDown } from '@ng-icons/lucide';
-import { BrnCollapsibleImports } from '@spartan-ng/brain/collapsible';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmCollapsibleImports } from '@spartan-ng/helm/collapsible';
 
 @Component({
 	selector: 'spartan-collapsible-preview',
-	imports: [BrnCollapsibleImports, HlmButtonImports, NgIcon],
+	imports: [HlmCollapsibleImports, HlmButtonImports, NgIcon],
 	providers: [provideIcons({ lucideChevronsUpDown })],
 	template: `
-		<brn-collapsible class="flex w-[350px] flex-col gap-2">
+		<hlm-collapsible class="flex w-[350px] flex-col gap-2">
 			<div class="flex items-center justify-between gap-4 px-4">
 				<h4 class="text-sm font-semibold">&#64;peduarte starred 3 repositories</h4>
-				<button brnCollapsibleTrigger hlmBtn variant="ghost" size="icon" class="size-8">
+				<button hlmCollapsibleTrigger hlmBtn variant="ghost" size="icon" class="size-8">
 					<ng-icon name="lucideChevronsUpDown" />
+					<span class="sr-only">Toggle</span>
 				</button>
 			</div>
 			<div class="rounded-md border px-4 py-2 font-mono text-sm">&#64;radix-ui/primitives</div>
-			<brn-collapsible-content class="flex flex-col gap-2">
+			<hlm-collapsible-content class="flex flex-col gap-2">
 				<div class="rounded-md border px-4 py-2 font-mono text-sm">&#64;radix-ui/colors</div>
 				<div class="rounded-md border px-4 py-2 font-mono text-sm">&#64;stitches/react</div>
-			</brn-collapsible-content>
-		</brn-collapsible>
+			</hlm-collapsible-content>
+		</hlm-collapsible>
 	`,
 })
 export class CollapsiblePreview {}
 
 export const defaultImports = `
-import { BrnCollapsibleImports } from '@spartan-ng/brain/collapsible';
+import { HlmCollapsibleImports } from '@spartan-ng/helm/collapsible';
 `;
 
 export const defaultSkeleton = `
-<brn-collapsible>
-  <button brnCollapsibleTrigger>Can I use this in my project?</button>
-    <brn-collapsible-content>
-    Yes. Free to use for personal and commercial projects. No attribution
-    required.
-    </brn-collapsible-content>
-</brn-collapsible>
+<hlm-collapsible>
+  <button hlmCollapsibleTrigger>Can I use this in my project?</button>
+  <hlm-collapsible-content> Yes. Free to use for personal and commercial projects. No attribution required. </hlm-collapsible-content>
+</hlm-collapsible>
 `;
--- libs/helm/collapsible/src/lib/hlm-collapsible-content.ts
+++ libs/helm/collapsible/src/lib/hlm-collapsible-content.ts
@@ -2,7 +2,7 @@ import { Directive } from '@angular/core';
 import { BrnCollapsibleContent } from '@spartan-ng/brain/collapsible';
 
 @Directive({
-	selector: '[hlmCollapsibleContent]',
+	selector: '[hlmCollapsibleContent],hlm-collapsible-content',
 	hostDirectives: [{ directive: BrnCollapsibleContent, inputs: ['id'] }],
 	host: {
 		'data-slot': 'collapsible-content',

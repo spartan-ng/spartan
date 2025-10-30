652b4f9fd8c4841b75ae9c29b270ebc08cfcba2a
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-10-30 10:29:29 +0100

    feat(collapsible): compute state, use disabled in toggle, calculate content size in afterNextRender

--- libs/brain/collapsible/src/lib/brn-collapsible-content.ts
+++ libs/brain/collapsible/src/lib/brn-collapsible-content.ts
@@ -1,35 +1,24 @@
-import { isPlatformServer } from '@angular/common';
-import {
-	Directive,
-	ElementRef,
-	type OnInit,
-	PLATFORM_ID,
-	effect,
-	inject,
-	input,
-	signal,
-	untracked,
-} from '@angular/core';
+import { Directive, ElementRef, afterNextRender, effect, inject, input, signal, untracked } from '@angular/core';
 import { injectBrnCollapsible } from './brn-collapsible-token';
 
 @Directive({
 	selector: '[brnCollapsibleContent],brn-collapsible-content',
 	host: {
 		'[hidden]': '!_collapsible?.expanded()',
-		'[attr.data-state]': '_collapsible?.expanded() ? "open" : "closed"',
+		'[attr.data-state]': '_collapsible?.state()',
 		'[id]': '_collapsible?.contentId()',
 		'[style.--brn-collapsible-content-width.px]': '_width()',
 		'[style.--brn-collapsible-content-height.px]': '_height()',
 	},
 })
-export class BrnCollapsibleContent implements OnInit {
+export class BrnCollapsibleContent {
 	protected readonly _collapsible = injectBrnCollapsible();
-	private readonly _elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
-	private readonly _platformId = inject(PLATFORM_ID);
+	private readonly _elementRef = inject<ElementRef>(ElementRef);
 	/**
 	 * The id of the collapsible content element.
 	 */
 	public readonly id = input<string | null | undefined>();
+
 	protected readonly _width = signal<number | null>(null);
 	protected readonly _height = signal<number | null>(null);
 
@@ -44,22 +33,18 @@ export class BrnCollapsibleContent implements OnInit {
 			if (!id || !collapsible) return;
 			untracked(() => collapsible.contentId.set(id));
 		});
-	}
 
-	ngOnInit(): void {
-		if (isPlatformServer(this._platformId)) {
-			return;
-		}
+		afterNextRender(() => {
+			// ensure the element is not hidden when measuring its size
+			this._elementRef.nativeElement.hidden = false;
 
-		// ensure the element is not hidden when measuring its size
-		this._elementRef.nativeElement.hidden = false;
+			const { width, height } = this._elementRef.nativeElement.getBoundingClientRect();
+			this._width.set(width);
+			this._height.set(height);
 
-		const { width, height } = this._elementRef.nativeElement.getBoundingClientRect();
-		this._width.set(width);
-		this._height.set(height);
-
-		// we force the element to be hidden again if collapsed after measuring its size
-		// this is handled by the host binding, but it can cause a flicker if we don't do this here manually
-		this._elementRef.nativeElement.hidden = this._collapsible?.expanded() ?? false;
+			// we force the element to be hidden again if collapsed after measuring its size
+			// this is handled by the host binding, but it can cause a flicker if we don't do this here manually
+			this._elementRef.nativeElement.hidden = !this._collapsible?.expanded();
+		});
 	}
 }
--- libs/brain/collapsible/src/lib/brn-collapsible-trigger.ts
+++ libs/brain/collapsible/src/lib/brn-collapsible-trigger.ts
@@ -1,18 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 import { injectBrnCollapsible } from './brn-collapsible-token';
 
 @Directive({
 	selector: 'button[brnCollapsibleTrigger]',
 	host: {
-		'[attr.data-state]': '_collapsible?.expanded() ? "open" : "closed"',
+		'[attr.data-state]': '_collapsible?.state()',
 		'[attr.disabled]': '_collapsible?.disabled() ? true : undefined',
 		'[attr.aria-expanded]': '_collapsible?.expanded()',
 		'[attr.aria-controls]': '_collapsible?.contentId()',
+		'[type]': 'type()',
 		'(click)': 'toggle()',
 	},
 })
 export class BrnCollapsibleTrigger {
 	protected readonly _collapsible = injectBrnCollapsible();
+
+	public readonly type = input<'button' | 'submit' | 'reset'>('button');
 
 	constructor() {
 		if (!this._collapsible) {

e21ebb9ab06466d5c9132f5d54d9f0456845676b
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-11-04 10:01:04 +0100

    feat(skeleton): change to directive, update styles and motion safe

--- libs/helm/skeleton/src/lib/hlm-skeleton.ts
+++ libs/helm/skeleton/src/lib/hlm-skeleton.ts
@@ -1,16 +1,17 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { computed, Directive, input } from '@angular/core';
 import { hlm } from '@spartan-ng/helm/utils';
 import type { ClassValue } from 'clsx';
 
-@Component({
-	selector: 'hlm-skeleton',
-	template: '',
+@Directive({
+	selector: '[hlmSkeleton],hlm-skeleton',
 	host: {
+		'data-slot': 'skeleton',
 		'[class]': '_computedClass()',
 	},
-	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HlmSkeleton {
 	public readonly userClass = input<ClassValue>('', { alias: 'class' });
-	protected readonly _computedClass = computed(() => hlm('bg-muted block animate-pulse rounded-md', this.userClass()));
+	protected readonly _computedClass = computed(() =>
+		hlm('bg-accent block rounded-md motion-safe:animate-pulse', this.userClass()),
+	);
 }

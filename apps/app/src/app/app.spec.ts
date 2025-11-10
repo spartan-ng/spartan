9df25d93a9d9651d9804949225789fbce932af97
robingotz <tug29225@temple.edu>
2025-11-10 14:44:09 +0100

    fix: dark-mode flashing on page - system preferences being ignored
    refactored the theme service to use signals and
    the code be more straight forward
    
    # Conflicts:
    #	apps/app/src/app/shared/theme.service.ts

--- apps/app/src/app/app.spec.ts
+++ apps/app/src/app/app.spec.ts
@@ -1,10 +1,22 @@
 import { provideHttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
+import { vitest } from 'vitest';
 import { App } from './app';
 
 describe('AppComponent', () => {
 	beforeEach(async () => {
+		Object.defineProperty(window, 'matchMedia', {
+			writable: true,
+			value: vitest.fn().mockImplementation((query) => ({
+				matches: false,
+				media: query,
+				addEventListener: vitest.fn(),
+				removeEventListener: vitest.fn(),
+				dispatchEvent: vitest.fn(),
+			})),
+		});
+
 		await TestBed.configureTestingModule({
 			providers: [provideRouter([]), provideHttpClient()],
 			imports: [App],

088b43d59b4d7fa4c0abca29f18acef17075f96d
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-11-12 13:12:27 +0100

    docs: project hlmSidebarInset as sibling to hlm-sidebar

--- apps/app/src/app/pages/(blocks-preview)/blocks-preview/sidebar-sticky-header/index.page.ts
+++ apps/app/src/app/pages/(blocks-preview)/blocks-preview/sidebar-sticky-header/index.page.ts
@@ -13,23 +13,19 @@ import { SiteHeader } from './sticky-header/site-header';
 	},
 	styleUrl: '../../blocks-preview-default.css',
 	template: `
-		<div hlmSidebarWrapper class="flex-col">
-			<spartan-site-header />
-
-			<div class="flex flex-1">
-				<spartan-app-sidebar />
-				<main hlmSidebarInset>
-					<div class="flex flex-1 flex-col gap-4 p-4">
-						<div class="grid auto-rows-min gap-4 md:grid-cols-3">
-							<div class="bg-muted/50 aspect-video rounded-xl"></div>
-							<div class="bg-muted/50 aspect-video rounded-xl"></div>
-							<div class="bg-muted/50 aspect-video rounded-xl"></div>
-						</div>
-						<div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"></div>
+		<spartan-app-sidebar-sticky>
+			<spartan-site-header header />
+			<main hlmSidebarInset>
+				<div class="flex flex-1 flex-col gap-4 p-4">
+					<div class="grid auto-rows-min gap-4 md:grid-cols-3">
+						<div class="bg-muted/50 aspect-video rounded-xl"></div>
+						<div class="bg-muted/50 aspect-video rounded-xl"></div>
+						<div class="bg-muted/50 aspect-video rounded-xl"></div>
 					</div>
-				</main>
-			</div>
-		</div>
+					<div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"></div>
+				</div>
+			</main>
+		</spartan-app-sidebar-sticky>
 	`,
 })
 export default class SidebarStickyHeaderPage {}
--- apps/app/src/app/pages/(blocks-preview)/blocks-preview/sidebar-sticky-header/sticky-header/app-sidebar.ts
+++ apps/app/src/app/pages/(blocks-preview)/blocks-preview/sidebar-sticky-header/sticky-header/app-sidebar.ts
@@ -9,39 +9,45 @@ import { NavSecondary } from './nav-secondary';
 import { NavUser } from './nav-user';
 
 @Component({
-	selector: 'spartan-app-sidebar',
+	selector: 'spartan-app-sidebar-sticky',
 	imports: [HlmSidebarImports, NgIcon, NavMain, NavProjects, NavUser, NavSecondary],
 	providers: [provideIcons({ lucideCommand })],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	template: `
-		<hlm-sidebar sidebarContainerClass="top-(--header-height) h-[calc(100svh-var(--header-height))]">
-			<hlm-sidebar-header>
-				<ul hlmSidebarMenu>
-					<li hlmSidebarMenuItem>
-						<a hlmSidebarMenuButton size="lg" href="#">
-							<div
-								class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg"
-							>
-								<ng-icon name="lucideCommand" class="text-base" />
-							</div>
-							<div class="grid flex-1 text-left text-sm leading-tight">
-								<span class="truncate font-medium">Acme Inc</span>
-								<span class="truncate text-xs">Enterprise</span>
-							</div>
-						</a>
-					</li>
-				</ul>
-			</hlm-sidebar-header>
+		<div hlmSidebarWrapper class="flex-col">
+			<ng-content select="[header]" />
+			<div class="flex flex-1">
+				<hlm-sidebar sidebarContainerClass="top-(--header-height) h-[calc(100svh-var(--header-height))]">
+					<hlm-sidebar-header>
+						<ul hlmSidebarMenu>
+							<li hlmSidebarMenuItem>
+								<a hlmSidebarMenuButton size="lg" href="#">
+									<div
+										class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg"
+									>
+										<ng-icon name="lucideCommand" class="text-base" />
+									</div>
+									<div class="grid flex-1 text-left text-sm leading-tight">
+										<span class="truncate font-medium">Acme Inc</span>
+										<span class="truncate text-xs">Enterprise</span>
+									</div>
+								</a>
+							</li>
+						</ul>
+					</hlm-sidebar-header>
 
-			<hlm-sidebar-content>
-				<spartan-nav-main [items]="data.navMain" />
-				<spartan-nav-projects />
-				<spartan-nav-secondary class="mt-auto" />
-			</hlm-sidebar-content>
-			<hlm-sidebar-footer>
-				<spartan-nav-user />
-			</hlm-sidebar-footer>
-		</hlm-sidebar>
+					<hlm-sidebar-content>
+						<spartan-nav-main [items]="data.navMain" />
+						<spartan-nav-projects />
+						<spartan-nav-secondary class="mt-auto" />
+					</hlm-sidebar-content>
+					<hlm-sidebar-footer>
+						<spartan-nav-user />
+					</hlm-sidebar-footer>
+				</hlm-sidebar>
+				<ng-content />
+			</div>
+		</div>
 	`,
 })
 export class AppSidebar {

144c3c392e6be03fb8a48a2732903ca803c8801b
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-11-13 19:15:21 +0100

    fix(collapsible): skip helm collapsible files in migration

--- libs/cli/src/generators/healthcheck/healthchecks/brn-collapsible.ts
+++ libs/cli/src/generators/healthcheck/healthchecks/brn-collapsible.ts
@@ -11,13 +11,22 @@ export const brainCollapsibleHealthcheck: Healthcheck = {
 				return;
 			}
 
+			// skip HlmCollapsible itself
+			if (
+				file.endsWith('hlm-collapsible.ts') ||
+				file.endsWith('hlm-collapsible-trigger.ts') ||
+				file.endsWith('hlm-collapsible-content.ts')
+			) {
+				return;
+			}
+
 			const contents = tree.read(file, 'utf-8');
 
 			if (!contents) {
 				return;
 			}
 
-			if (contents.includes("'@spartan-ng/brain/collapsible'")) {
+			if (contents.includes("'@spartan-ng/brain/collapsible'") || contents.includes('brn-collapsible')) {
 				failure(
 					`The <brn-collapsible> component is deprecated. Please use the <hlm-collapsible> instead.`,
 					HealthcheckSeverity.Error,

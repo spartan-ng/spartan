641577335b03131cb2e5f7cf2ccd2af8e0e7a826
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-11-17 14:34:15 +0100

    feat: button group polishing (#1028)
    * feat(button-group): update selectors
    
    * docs(button-group): update imports, add input-group example

--- apps/app/src/app/pages/(components)/components/(button-group)/button-group--input.example.ts
+++ apps/app/src/app/pages/(components)/components/(button-group)/button-group--input.example.ts
@@ -1,22 +1,18 @@
 import { Component } from '@angular/core';
-import { NgIcon, provideIcons } from '@ng-icons/core';
+import { provideIcons } from '@ng-icons/core';
 import { lucideSearch } from '@ng-icons/lucide';
-import { HlmButton } from '@spartan-ng/helm/button';
-import { HlmButtonGroup } from '@spartan-ng/helm/button-group';
-import { HlmIcon } from '@spartan-ng/helm/icon';
-import { HlmInput } from '@spartan-ng/helm/input';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmButtonGroupImports } from '@spartan-ng/helm/button-group';
+import { HlmIconImports } from '@spartan-ng/helm/icon';
+import { HlmInputImports } from '@spartan-ng/helm/input';
 
 @Component({
 	selector: 'spartan-button-group-input',
-	imports: [HlmIcon, NgIcon, HlmButton, HlmButtonGroup, HlmInput],
-	providers: [
-		provideIcons({
-			lucideSearch,
-		}),
-	],
+	imports: [HlmIconImports, HlmButtonImports, HlmButtonGroupImports, HlmInputImports],
+	providers: [provideIcons({ lucideSearch })],
 	template: `
 		<div hlmButtonGroup>
-			<input hlmInput placeholder="Search..." class="z-10" />
+			<input hlmInput placeholder="Search..." />
 			<button hlmBtn variant="outline">
 				<ng-icon hlm name="lucideSearch" size="sm" />
 			</button>
--- apps/app/src/app/pages/(components)/components/(button-group)/button-group--orientation.example.ts
+++ apps/app/src/app/pages/(components)/components/(button-group)/button-group--orientation.example.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core';
-import { NgIcon, provideIcons } from '@ng-icons/core';
+import { provideIcons } from '@ng-icons/core';
 import { lucideMinus, lucidePlus } from '@ng-icons/lucide';
-import { HlmButton } from '@spartan-ng/helm/button';
-import { HlmButtonGroup } from '@spartan-ng/helm/button-group';
-import { HlmIcon } from '@spartan-ng/helm/icon';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmButtonGroupImports } from '@spartan-ng/helm/button-group';
+import { HlmIconImports } from '@spartan-ng/helm/icon';
 
 @Component({
 	selector: 'spartan-button-group-orientation',
-	imports: [NgIcon, HlmIcon, HlmButton, HlmButtonGroup],
-	providers: [
-		provideIcons({
-			lucidePlus,
-			lucideMinus,
-		}),
-	],
+	imports: [HlmIconImports, HlmButtonImports, HlmButtonGroupImports],
+	providers: [provideIcons({ lucidePlus, lucideMinus })],
 	template: `
-		<div hlmButtonGroup orientation="vertical" class="mb-4">
+		<div hlmButtonGroup orientation="vertical" aria-label="Media controls" class="h-fit">
 			<button hlmBtn variant="outline" size="icon">
 				<ng-icon hlm name="lucidePlus" size="sm" />
 			</button>

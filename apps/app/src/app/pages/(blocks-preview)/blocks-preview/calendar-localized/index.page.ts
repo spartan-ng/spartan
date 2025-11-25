8a27bf01e55c6d6009315c6fa6388ad3492287a9
Linh Pham <linh.pham@oldendorff.com>
2025-11-25 02:28:45 +0100

    refactor(calendar): move height css to calendar directly

--- apps/app/src/app/pages/(blocks-preview)/blocks-preview/calendar-localized/index.page.ts
+++ apps/app/src/app/pages/(blocks-preview)/blocks-preview/calendar-localized/index.page.ts
@@ -170,9 +170,7 @@ export class LanguageService {
 			</div>
 
 			<div hlmCardContent class="flex flex-row gap-3 px-6">
-				<div class="h-80">
-					<hlm-calendar class="h-full" />
-				</div>
+				<hlm-calendar class='h-80' />
 
 				<div class="no-scrollbar flex h-80 scroll-pb-6 flex-col gap-2 overflow-y-auto">
 					@for (time of _timeSlots; track time) {

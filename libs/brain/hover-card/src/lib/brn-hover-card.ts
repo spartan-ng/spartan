fec57ce6d1ea5dd3625722a4f90d9e391b0d7a1a
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-12-04 12:49:41 +0100

    feat(hover-card): use directive

--- libs/brain/hover-card/src/lib/brn-hover-card.ts
+++ libs/brain/hover-card/src/lib/brn-hover-card.ts
@@ -1,13 +1,9 @@
-import { type AfterContentInit, ChangeDetectionStrategy, Component, contentChild } from '@angular/core';
+import { type AfterContentInit, contentChild, Directive } from '@angular/core';
 import { BrnHoverCardContent, BrnHoverCardContentService, BrnHoverCardTrigger } from './brn-hover-card-content.service';
 
-@Component({
-	selector: 'brn-hover-card',
+@Directive({
+	selector: '[brnHoverCard],brn-hover-card',
 	providers: [BrnHoverCardContentService],
-	changeDetection: ChangeDetectionStrategy.OnPush,
-	template: `
-		<ng-content />
-	`,
 })
 export class BrnHoverCard implements AfterContentInit {
 	private readonly _trigger = contentChild(BrnHoverCardTrigger);

b013fcd717c1fe1c1342297f10e32867edf3d6a2
Raielly Souza <rrayzb04@gmail.com>
2025-12-13 12:17:04 -0300

    fix(brain/alert-dialog): avoid IIC by using forwardRef in provider

--- spartan/libs/brain/alert-dialog/src/lib/brn-alert-dialog.ts
+++ spartan/libs/brain/alert-dialog/src/lib/brn-alert-dialog.ts
@@ -2,20 +2,21 @@ import { Directive, forwardRef } from '@angular/core';
 import { BrnDialog, type BrnDialogDefaultOptions, provideBrnDialogDefaultOptions } from '@spartan-ng/brain/dialog';
 
 export const BRN_ALERT_DIALOG_DEFAULT_OPTIONS: Partial<BrnDialogDefaultOptions> = {
-	closeOnBackdropClick: false,
-	closeOnOutsidePointerEvents: false,
-	role: 'alertdialog',
+    closeOnBackdropClick: false,
+    closeOnOutsidePointerEvents: false,
+    role: 'alertdialog',
 };
 
 @Directive({
-	selector: '[brnAlertDialog],brn-alert-dialog',
-	exportAs: 'brnAlertDialog',
-	providers: [
-		{
-			provide: BrnDialog,
-			useExisting: forwardRef(() => BrnAlertDialog),
-		},
-		provideBrnDialogDefaultOptions(BRN_ALERT_DIALOG_DEFAULT_OPTIONS),
-	],
+    selector: '[brnAlertDialog],brn-alert-dialog',
+    exportAs: 'brnAlertDialog',
+    providers: [
+        {
+            provide: BrnDialog,
+            // resolve the existing instance lazily to break circular initialization
+            useExisting: forwardRef(() => BrnAlertDialog),
+        },
+        provideBrnDialogDefaultOptions(BRN_ALERT_DIALOG_DEFAULT_OPTIONS),
+    ],
 })
 export class BrnAlertDialog extends BrnDialog {}

9ecdbf062d2a7cb4c0b5b2d617e7c5b268090a60
Raielly Souza <rrayzb04@gmail.com>
2025-12-13 11:49:38 -0300

    fix(brain/alert-dialog): avoid IIC by using forwardRef in provider

--- spartan/libs/brain/alert-dialog/src/lib/brn-alert-dialog-content.ts
+++ spartan/libs/brain/alert-dialog/src/lib/brn-alert-dialog-content.ts
@@ -1,9 +1,11 @@
-import { Directive } from '@angular/core';
+import { Directive, forwardRef } from '@angular/core';
 import { provideExposesStateProviderExisting } from '@spartan-ng/brain/core';
 import { BrnDialogContent } from '@spartan-ng/brain/dialog';
 
 @Directive({
-	selector: '[brnAlertDialogContent]',
-	providers: [provideExposesStateProviderExisting(() => BrnAlertDialogContent)],
+    selector: '[brnAlertDialogContent]',
+    providers: [
+        provideExposesStateProviderExisting(() => forwardRef(() => BrnAlertDialogContent)),
+    ],
 })
 export class BrnAlertDialogContent<T> extends BrnDialogContent<T> {}

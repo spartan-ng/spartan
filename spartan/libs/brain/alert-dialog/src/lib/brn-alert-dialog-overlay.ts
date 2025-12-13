b1e944e184f9f0aeb9c176208cb6b5d4e5725dcd
Raielly Souza <rrayzb04@gmail.com>
2025-12-13 12:04:30 -0300

    fix(brain/alert-dialog): use forwardRef in provider to avoid IIC

--- spartan/libs/brain/alert-dialog/src/lib/brn-alert-dialog-overlay.ts
+++ spartan/libs/brain/alert-dialog/src/lib/brn-alert-dialog-overlay.ts
@@ -1,9 +1,9 @@
-import { Directive } from '@angular/core';
+import { Directive, forwardRef } from '@angular/core';
 import { provideCustomClassSettableExisting } from '@spartan-ng/brain/core';
 import { BrnDialogOverlay } from '@spartan-ng/brain/dialog';
 
 @Directive({
-	selector: '[brnAlertDialogOverlay],brn-alert-dialog-overlay',
-	providers: [provideCustomClassSettableExisting(() => BrnAlertDialogOverlay)],
+    selector: '[brnAlertDialogOverlay],brn-alert-dialog-overlay',
+    providers: [provideCustomClassSettableExisting(() => forwardRef(() => BrnAlertDialogOverlay))],
 })
 export class BrnAlertDialogOverlay extends BrnDialogOverlay {}

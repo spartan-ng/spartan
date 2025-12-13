791e55b517acc30deefa581cda5d293c4dc8b3b8
Raielly Souza <rrayzb04@gmail.com>
2025-12-13 12:23:17 -0300

    fix(brain/popover): avoid IIC by using forwardRef in provider

--- spartan/libs/brain/popover/src/lib/brn-popover-content.ts
+++ spartan/libs/brain/popover/src/lib/brn-popover-content.ts
@@ -1,9 +1,9 @@
-import { Directive } from '@angular/core';
+import { Directive, forwardRef } from '@angular/core';
 import { provideExposesStateProviderExisting } from '@spartan-ng/brain/core';
 import { BrnDialogContent } from '@spartan-ng/brain/dialog';
 
 @Directive({
-	selector: '[brnPopoverContent]',
-	providers: [provideExposesStateProviderExisting(() => BrnPopoverContent)],
+    selector: '[brnPopoverContent]',
+    providers: [provideExposesStateProviderExisting(() => forwardRef(() => BrnPopoverContent))],
 })
 export class BrnPopoverContent<T> extends BrnDialogContent<T> {}

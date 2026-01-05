34531de3f964d4589d7689081f631478879789c0
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-01-05 12:51:58 +0100

    feat(command): update styles

--- libs/helm/command/src/lib/hlm-command-group-label.ts
+++ libs/helm/command/src/lib/hlm-command-group-label.ts
@@ -10,6 +10,6 @@ import { classes } from '@spartan-ng/helm/utils';
 })
 export class HlmCommandGroupLabel {
 	constructor() {
-		classes(() => 'text-muted-foreground px-2 py-1.5 text-xs font-medium');
+		classes(() => 'block text-muted-foreground px-2 py-1.5 text-xs font-medium');
 	}
 }

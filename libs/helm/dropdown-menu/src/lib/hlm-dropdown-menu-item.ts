06efda1a4246b7ea4dfa0e4d6b770a4b6baf84d4
Felix Hagemans <62592273+felhag@users.noreply.github.com>
2026-01-09 09:37:12 +0100

    fix(dropdown-menu): only add disabled attribute for buttons

--- libs/helm/dropdown-menu/src/lib/hlm-dropdown-menu-item.ts
+++ libs/helm/dropdown-menu/src/lib/hlm-dropdown-menu-item.ts
@@ -1,6 +1,6 @@
 import { type BooleanInput } from '@angular/cdk/coercion';
 import { CdkMenuItem } from '@angular/cdk/menu';
-import { booleanAttribute, Directive, input } from '@angular/core';
+import { booleanAttribute, Directive, HOST_TAG_NAME, inject, input } from '@angular/core';
 import { classes } from '@spartan-ng/helm/utils';
 
 @Directive({
@@ -14,13 +14,15 @@ import { classes } from '@spartan-ng/helm/utils';
 	],
 	host: {
 		'data-slot': 'dropdown-menu-item',
-		'[attr.disabled]': 'disabled() || null',
+		'[attr.disabled]': '_isButton && disabled() ? "" : null',
 		'[attr.data-disabled]': 'disabled() ? "" : null',
 		'[attr.data-variant]': 'variant()',
 		'[attr.data-inset]': 'inset() ? "" : null',
 	},
 })
 export class HlmDropdownMenuItem {
+	protected readonly _isButton = inject(HOST_TAG_NAME) === 'button';
+
 	public readonly disabled = input<boolean, BooleanInput>(false, { transform: booleanAttribute });
 
 	public readonly variant = input<'default' | 'destructive'>('default');

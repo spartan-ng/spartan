de3ee56fc490ab82fd0455083b89fe741c7fc832
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-01-12 14:46:17 +0100

    chore(cli): generate combobox

--- libs/cli/src/generators/ui/libs/combobox/generator.ts
+++ libs/cli/src/generators/ui/libs/combobox/generator.ts
@@ -3,8 +3,5 @@ import hlmBaseGenerator from '../../../base/generator';
 import type { HlmBaseGeneratorSchema } from '../../../base/schema';
 
 export async function generator(tree: Tree, options: HlmBaseGeneratorSchema) {
-	return await hlmBaseGenerator(tree, {
-		...options,
-		name: 'combobox',
-	});
+	return await hlmBaseGenerator(tree, { ...options, name: 'combobox' });
 }

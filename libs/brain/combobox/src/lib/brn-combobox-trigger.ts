5b5cc96874c513da84d6210dee8e1e315195ee60
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-01-16 09:47:58 +0100

    fix(combobox): set brnComboboxTrigger type to button, avoid form submission

--- libs/brain/combobox/src/lib/brn-combobox-trigger.ts
+++ libs/brain/combobox/src/lib/brn-combobox-trigger.ts
@@ -2,8 +2,9 @@ import { Directive } from '@angular/core';
 import { injectBrnComboboxBase } from './brn-combobox.token';
 
 @Directive({
-	selector: '[brnComboboxTrigger]',
+	selector: 'button[brnComboboxTrigger]',
 	host: {
+		type: 'button',
 		'[attr.aria-disabled]': '_disabled() ? "true" : null',
 		'[attr.disabled]': '_disabled() ? "" : null',
 	},

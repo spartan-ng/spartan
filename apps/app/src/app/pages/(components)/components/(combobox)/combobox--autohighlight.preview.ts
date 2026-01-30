3826446aaf4f863470a01ee83ce013aa6df530b8
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-01-30 11:54:34 +0100

    docs(combobox): self closing for combobox-input

--- apps/app/src/app/pages/(components)/components/(combobox)/combobox--autohighlight.preview.ts
+++ apps/app/src/app/pages/(components)/components/(combobox)/combobox--autohighlight.preview.ts
@@ -7,7 +7,7 @@ import { HlmComboboxImports } from '@spartan-ng/helm/combobox';
 	imports: [HlmComboboxImports, BrnPopoverContent],
 	template: `
 		<hlm-combobox autoHighlight>
-			<hlm-combobox-input placeholder="Select a framework"></hlm-combobox-input>
+			<hlm-combobox-input placeholder="Select a framework" />
 			<div *brnPopoverContent hlmComboboxContent>
 				<hlm-combobox-empty>No items found.</hlm-combobox-empty>
 				<div hlmComboboxList>
@@ -47,38 +47,3 @@ export class ComboboxAutoHighlightPreview {
 		},
 	];
 }
-
-export const defaultImports = `
-import { BrnPopoverContent } from '@spartan-ng/brain/popover';
-import { HlmComboboxImports } from '@spartan-ng/helm/combobox';
-`;
-
-export const defaultSkeleton = `
-<hlm-combobox>
-  <hlm-combobox-input placeholder="Select a framework"></hlm-combobox-input>
-  <div *brnPopoverContent hlmComboboxContent>
-    <hlm-combobox-empty>No items found.</hlm-combobox-empty>
-    <div hlmComboboxList>
-      @for (framework of frameworks; track $index) {
-      	<hlm-combobox-item [value]="framework">{{ framework.label }}</hlm-combobox-item>
-      }
-    </div>
-  </div>
-</hlm-combobox>
-`;
-
-export const comboboxDefaultConfig = `
-import { comboboxContainsFilter, provideBrnComboboxConfig } from '@spartan-ng/brain/combobox';
-
-provideBrnComboboxConfig({
-	filterOptions: {
-		usage: 'search',
-		sensitivity: 'base',
-		ignorePunctuation: true,
-	},
-	filter: (itemValue: T, search: string, collator: Intl.Collator, itemToString?: ComboboxItemToString<T>) =>
-		comboboxContainsFilter(itemValue, search, collator, itemToString),
-	isItemEqualToValue: (itemValue: T, selectedValue: T | null) => Object.is(itemValue, selectedValue),
-	itemToString: undefined,
-});
-`;

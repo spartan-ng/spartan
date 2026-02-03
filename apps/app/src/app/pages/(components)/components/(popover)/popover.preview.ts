1d15d77c61a43a70f99007b9a725b297587d6743
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-02-03 10:34:14 +0100

    feat(popover): add hlmPopoverPortal to expose *brnPopoverContent

--- apps/app/src/app/pages/(components)/components/(popover)/popover.preview.ts
+++ apps/app/src/app/pages/(components)/components/(popover)/popover.preview.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { BrnPopoverImports } from '@spartan-ng/brain/popover';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmInputImports } from '@spartan-ng/helm/input';
 import { HlmLabelImports } from '@spartan-ng/helm/label';
@@ -7,11 +6,11 @@ import { HlmPopoverImports } from '@spartan-ng/helm/popover';
 
 @Component({
 	selector: 'spartan-popover-preview',
-	imports: [BrnPopoverImports, HlmPopoverImports, HlmButtonImports, HlmLabelImports, HlmInputImports],
+	imports: [HlmPopoverImports, HlmButtonImports, HlmLabelImports, HlmInputImports],
 	template: `
 		<hlm-popover sideOffset="5">
 			<button id="edit-profile" hlmPopoverTrigger hlmBtn variant="outline">Open Popover</button>
-			<div hlmPopoverContent class="grid w-80 gap-4" *brnPopoverContent="let ctx">
+			<div hlmPopoverContent class="grid w-80 gap-4" *hlmPopoverPortal="let ctx">
 				<div class="space-y-2">
 					<h4 class="leading-none font-medium">Dimensions</h4>
 					<p class="text-muted-foreground text-sm">Set the dimensions for the layer.</p>
@@ -41,12 +40,12 @@ import { HlmPopoverImports } from '@spartan-ng/helm/popover';
 export class PopoverPreview {}
 
 export const defaultImports = `
-import { BrnPopoverImports } from '@spartan-ng/brain/popover';
 import { HlmPopoverImports } from '@spartan-ng/helm/popover';
 `;
+
 export const defaultSkeleton = `
 <hlm-popover>
   <button hlmPopoverTrigger>Open Popover</button>
-  <div hlmPopoverContent *brnPopoverContent="let ctx"></div>
+  <div hlmPopoverContent *hlmPopoverPortal="let ctx"></div>
 </hlm-popover>
 `;

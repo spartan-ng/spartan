29dcef0c19f52b155ca92efc1d230bb4e9f297a0
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-02-03 10:30:29 +0100

    feat(alert-dialog): add hlmAlertDialogPortal to expose *brnAlertDialogContent

--- apps/app/src/app/pages/(components)/components/(alert-dialog)/alert-dialog.preview.ts
+++ apps/app/src/app/pages/(components)/components/(alert-dialog)/alert-dialog.preview.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { BrnAlertDialogImports } from '@spartan-ng/brain/alert-dialog';
 import { HlmAlertDialogImports } from '@spartan-ng/helm/alert-dialog';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 
 @Component({
 	selector: 'spartan-alert-dialog-preview',
-	imports: [BrnAlertDialogImports, HlmAlertDialogImports, HlmButtonImports],
+	imports: [HlmAlertDialogImports, HlmButtonImports],
 	template: `
 		<hlm-alert-dialog>
 			<button id="edit-profile" hlmAlertDialogTrigger hlmBtn variant="outline">Show Dialog</button>
-			<hlm-alert-dialog-content *brnAlertDialogContent="let ctx">
+			<hlm-alert-dialog-content *hlmAlertDialogPortal="let ctx">
 				<hlm-alert-dialog-header>
 					<h2 hlmAlertDialogTitle>Are you absolutely sure?</h2>
 					<p hlmAlertDialogDescription>
@@ -28,14 +27,13 @@ import { HlmButtonImports } from '@spartan-ng/helm/button';
 export class AlertDialogPreview {}
 
 export const defaultImports = `
-import { BrnAlertDialogImports } from '@spartan-ng/brain/alert-dialog';
 import { HlmAlertDialogImports } from '@spartan-ng/helm/alert-dialog';
 `;
 
 export const defaultSkeleton = `
 <hlm-alert-dialog>
   <button id="edit-profile" hlmAlertDialogTrigger>Show Dialog</button>
-  <hlm-alert-dialog-content *brnAlertDialogContent="let ctx">
+  <hlm-alert-dialog-content *hlmAlertDialogPortal="let ctx">
     <hlm-alert-dialog-header>
       <h2 hlmAlertDialogTitle>Are you absolutely sure?</h2>
       <p hlmAlertDialogDescription>

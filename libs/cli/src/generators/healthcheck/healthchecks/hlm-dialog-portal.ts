d0a48201b21591a011b343ba6f4e9bcff642f16a
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-02-03 11:50:16 +0100

    chore: remove importAlias in healthcheck

--- libs/cli/src/generators/healthcheck/healthchecks/hlm-dialog-portal.ts
+++ libs/cli/src/generators/healthcheck/healthchecks/hlm-dialog-portal.ts
@@ -4,7 +4,7 @@ import { HealthcheckSeverity, type Healthcheck } from '../healthchecks';
 
 export const helmDialogPortalHealthcheck: Healthcheck = {
 	name: 'Helm Dialog Portal',
-	async detect(tree, failure, _, { importAlias }) {
+	async detect(tree, failure) {
 		visitNotIgnoredFiles(tree, '/', (file) => {
 			// if the file is a .ts or .htlm file, check for helm menu
 			if (!file.endsWith('.ts') && !file.endsWith('.html')) {

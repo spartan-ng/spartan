6a1ab4b2c1764b599b5046dde4c5f0c827f31679
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-02-04 10:17:15 +0100

    feat(sheet): add showCloseButton input and use hlmBtn for close button

--- libs/helm/sheet/src/lib/hlm-sheet-close.ts
+++ libs/helm/sheet/src/lib/hlm-sheet-close.ts
@@ -11,9 +11,6 @@ import { classes } from '@spartan-ng/helm/utils';
 })
 export class HlmSheetClose {
 	constructor() {
-		classes(
-			() =>
-				'ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none',
-		);
+		classes(() => 'absolute top-4 right-4');
 	}
 }

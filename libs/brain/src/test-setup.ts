7d09f26fa8a5bf9515cdb7abfd7f32d041b7d517
Toni Škobić <toni.skobic@gmail.com>
2026-02-07 02:30:52 +0100

    test(slider): fix form tests

--- libs/brain/src/test-setup.ts
+++ libs/brain/src/test-setup.ts
@@ -4,5 +4,13 @@ import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 setupZoneTestEnv();
 
 import { toHaveNoViolations } from 'jest-axe';
-
 expect.extend(toHaveNoViolations);
+
+// ---- ResizeObserver mock for Angular tests ----
+class ResizeObserverMock {
+	observe() {}
+	unobserve() {}
+	disconnect() {}
+}
+
+globalThis.ResizeObserver = ResizeObserverMock;

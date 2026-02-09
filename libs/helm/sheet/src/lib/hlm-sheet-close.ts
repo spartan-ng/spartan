67f1b5e53d904ff10595951d35014015d5ee17f4
Merlin <merlin.moos@oldendorff.com>
2026-02-09 17:04:33 +0100

    fix: e2e

--- libs/helm/sheet/src/lib/hlm-sheet-close.ts
+++ libs/helm/sheet/src/lib/hlm-sheet-close.ts
@@ -4,5 +4,8 @@ import { BrnSheetClose } from '@spartan-ng/brain/sheet';
 @Directive({
 	selector: 'button[hlmSheetClose]',
 	hostDirectives: [{ directive: BrnSheetClose, inputs: ['delay'] }],
+	host: {
+		'data-slot': 'sheet-close',
+	},
 })
 export class HlmSheetClose {}

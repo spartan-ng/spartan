a886a46be6b9895e3cdd6f9046d7dfeb26b79390
elite-benni <benjamin.heiss@elite.tirol>
2026-02-15 00:33:10 +0100

    test(slider): add test to verify different IDs are generated

--- libs/brain/slider/src/lib/__tests__/brn-slider-accessibility.spec.ts
+++ libs/brain/slider/src/lib/__tests__/brn-slider-accessibility.spec.ts
@@ -46,6 +46,26 @@ class SliderWithAriaLabel {}
 })
 class SliderWithDefaultAriaLabels {}
 
+@Component({
+	imports: [BrnSlider, BrnSliderThumb, BrnSliderTrack, BrnSliderRange],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	template: `
+		<div brnSlider [value]="[30]">
+			<div brnSliderTrack>
+				<div brnSliderRange></div>
+			</div>
+			<span brnSliderThumb></span>
+		</div>
+		<div brnSlider [value]="[70]">
+			<div brnSliderTrack>
+				<div brnSliderRange></div>
+			</div>
+			<span brnSliderThumb></span>
+		</div>
+	`,
+})
+class TwoSliders {}
+
 describe('Slider Accessibility', () => {
 	it('should forward aria-labelledby from slider to thumb', async () => {
 		const { container } = await render(SliderWithAriaLabelledby);
@@ -74,5 +94,16 @@ describe('Slider Accessibility', () => {
 		const thumb = container.querySelector('[role="slider"]') as HTMLElement;
 
 		expect(thumb.getAttribute('aria-labelledby')).toBeNull();
+	});
+
+	it('should generate unique IDs for multiple slider instances', async () => {
+		const { container } = await render(TwoSliders);
+		const sliders = container.querySelectorAll('[data-slot="slider"]');
+		const id1 = sliders[0].getAttribute('id');
+		const id2 = sliders[1].getAttribute('id');
+
+		expect(id1).toBeTruthy();
+		expect(id2).toBeTruthy();
+		expect(id1).not.toBe(id2);
 	});
 });

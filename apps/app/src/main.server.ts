c81a29dce3cfa270f2a23036d5d0062dc0ee7202
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-02-26 09:47:43 +0100

    chore: update nx and angular 20.3
    # Conflicts:
    #	pnpm-lock.yaml

--- apps/app/src/main.server.ts
+++ apps/app/src/main.server.ts
@@ -4,7 +4,7 @@ import 'zone.js/node';
 import { provideServerContext } from '@analogjs/router/server';
 import type { ServerContext } from '@analogjs/router/tokens';
 import { enableProdMode } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { renderApplication } from '@angular/platform-server';
 import { config } from './app.config.server';
 import { App } from './app/app';
@@ -12,7 +12,7 @@ import { App } from './app/app';
 if (import.meta.env.PROD) {
 	enableProdMode();
 }
-const bootstrap = () => bootstrapApplication(App, config);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(App, config, context);
 export default async function render(url: string, document: string, serverContext: ServerContext) {
 	const html = await renderApplication(bootstrap, {
 		document,

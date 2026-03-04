3a192ebee7983d6937c8108829e39f4d33c728bf
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-03-04 11:40:13 +0100

    docs(sonner): move default template

---
+++ apps/app/src/app/pages/(components)/components/(sonner)/sonner-template.ts
@@ -0,0 +1,17 @@
+export const defaultTemplate = `
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { HlmToasterImports } from '@spartan-ng/helm/sonner';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HlmToasterImports],
+  template: \`
+    <router-outlet />
+
+    <hlm-toaster />
+  \`,
+})
+export class App {}
+`;

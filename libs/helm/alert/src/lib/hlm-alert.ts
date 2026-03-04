1cd3e58fcd7d0a3c7d40dfb8f2e92ce04b37692c
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-03-04 17:01:32 +0100

    fix(alert): target ng-icon in title and simplify alert classes

--- libs/helm/alert/src/lib/hlm-alert.ts
+++ libs/helm/alert/src/lib/hlm-alert.ts
@@ -3,13 +3,12 @@ import { classes } from '@spartan-ng/helm/utils';
 import { type VariantProps, cva } from 'class-variance-authority';
 
 const alertVariants = cva(
-	"group/alert relative grid w-full gap-0.5 rounded-lg border px-4 py-3 text-left text-sm has-data-[slot=alert-action]:relative has-data-[slot=alert-action]:pr-18 has-[>ng-icon]:grid-cols-[auto_1fr] has-[>ng-icon]:gap-x-2.5 *:[ng-icon]:row-span-2 *:[ng-icon]:translate-y-0.5 *:[ng-icon]:text-current *:[ng-icon:not([class*='text-'])]:text-base",
+	"group/alert relative grid w-full gap-0.5 rounded-lg border px-4 py-3 text-left text-sm has-data-[slot=alert-action]:pr-18 has-[>ng-icon]:grid-cols-[auto_1fr] has-[>ng-icon]:gap-x-2.5 *:[ng-icon]:row-span-2 *:[ng-icon]:translate-y-0.5 *:[ng-icon]:text-current *:[ng-icon:not([class*='text-'])]:text-base",
 	{
 		variants: {
 			variant: {
 				default: 'bg-card text-card-foreground',
-				destructive:
-					'text-destructive bg-card *:data-[slot=alert-description]:text-destructive/90 *:[ng-icon]:text-current',
+				destructive: 'text-destructive bg-card *:data-[slot=alert-description]:text-destructive/90',
 			},
 		},
 		defaultVariants: {

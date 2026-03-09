4141f0d27cfa08584de9adc668b8ed9bd40c67fc
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-03-09 19:01:51 +0100

    docs: revert spartan preset (#1265)

--- apps/app/src/app/pages/(documentation)/documentation/(ui)/installation.server.ts
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from 'fs';
-
-export const load = async () => {
-	const spartanPreset = readFileSync(`./libs/brain/hlm-tailwind-preset.css`, 'utf-8');
-
-	return { spartanPreset };
-};

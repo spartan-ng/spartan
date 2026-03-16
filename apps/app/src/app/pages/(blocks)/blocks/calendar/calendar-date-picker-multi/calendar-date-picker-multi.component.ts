488015e2d1b81e3ffd171741e7e233d98c497354
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-03-16 12:39:11 +0100

    docs: use hlm-field for date-picker blocks

--- apps/app/src/app/pages/(blocks)/blocks/calendar/calendar-date-picker-multi/calendar-date-picker-multi.component.ts
+++ apps/app/src/app/pages/(blocks)/blocks/calendar/calendar-date-picker-multi/calendar-date-picker-multi.component.ts
@@ -1,34 +1,37 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { HlmButton } from '@spartan-ng/helm/button';
-import { HlmDatePickerMulti } from '@spartan-ng/helm/date-picker';
-import { HlmLabel } from '@spartan-ng/helm/label';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmDatePickerImports } from '@spartan-ng/helm/date-picker';
+import { HlmFieldImports } from '@spartan-ng/helm/field';
 
 @Component({
 	selector: 'spartan-calendar-date-picker-multi',
-	imports: [HlmDatePickerMulti, ReactiveFormsModule, HlmButton, HlmLabel],
+	imports: [ReactiveFormsModule, HlmDatePickerImports, HlmButtonImports, HlmFieldImports],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	host: {
 		class: 'flex justify-center p-3 md:p-20 w-full h-auto md:h-[600px] bg-surface rounded-lg overflow-auto',
 	},
 	template: `
-		<form [formGroup]="form" (ngSubmit)="submit()" class="space-y-8">
-			<div class="flex flex-col gap-2">
-				<label for="availableDates" hlmLabel class="px-1">Available dates</label>
-				<hlm-date-picker-multi
-					buttonId="availableDates"
-					[min]="minDate"
-					[max]="maxDate"
-					formControlName="availableDates"
-					[autoCloseOnMaxSelection]="true"
-					[minSelection]="2"
-					[maxSelection]="4"
-				>
-					<span>Pick dates</span>
-				</hlm-date-picker-multi>
-			</div>
-
-			<button type="submit" hlmBtn [disabled]="form.invalid">Submit</button>
+		<form [formGroup]="form" (ngSubmit)="submit()" class="w-full max-w-xs">
+			<hlm-field-group>
+				<hlm-field>
+					<label hlmFieldLabel for="availableDates">Available dates</label>
+					<hlm-date-picker-multi
+						buttonId="availableDates"
+						[min]="minDate"
+						[max]="maxDate"
+						formControlName="availableDates"
+						[autoCloseOnMaxSelection]="true"
+						[minSelection]="2"
+						[maxSelection]="4"
+					>
+						<span>Pick dates</span>
+					</hlm-date-picker-multi>
+				</hlm-field>
+				<hlm-field orientation="horizontal">
+					<button type="submit" hlmBtn [disabled]="form.invalid">Submit</button>
+				</hlm-field>
+			</hlm-field-group>
 		</form>
 	`,
 })
--- apps/app/src/app/pages/(blocks)/blocks/calendar/calendar-date-picker-range/calendar-date-picker-range.component.ts
+++ apps/app/src/app/pages/(blocks)/blocks/calendar/calendar-date-picker-range/calendar-date-picker-range.component.ts
@@ -1,32 +1,35 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { HlmButton } from '@spartan-ng/helm/button';
-import { HlmDateRangePicker } from '@spartan-ng/helm/date-picker';
-import { HlmLabel } from '@spartan-ng/helm/label';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmDatePickerImports } from '@spartan-ng/helm/date-picker';
+import { HlmFieldImports } from '@spartan-ng/helm/field';
 
 @Component({
 	selector: 'spartan-calendar-date-picker-range',
-	imports: [HlmDateRangePicker, ReactiveFormsModule, HlmLabel, HlmButton],
+	imports: [ReactiveFormsModule, HlmDatePickerImports, HlmFieldImports, HlmButtonImports],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	host: {
 		class: 'flex justify-center p-3 md:p-20 w-full h-auto md:h-[600px] bg-surface rounded-lg overflow-auto',
 	},
 	template: `
-		<form [formGroup]="form" (ngSubmit)="submit()" class="space-y-8">
-			<div class="flex flex-col gap-2">
-				<label for="dateRange" hlmLabel class="px-1">Enter a date range</label>
-				<hlm-date-range-picker
-					buttonId="dateRange"
-					[min]="minDate"
-					[max]="maxDate"
-					formControlName="range"
-					[autoCloseOnEndSelection]="true"
-				>
-					<span>Enter a date range</span>
-				</hlm-date-range-picker>
-			</div>
-
-			<button type="submit" hlmBtn [disabled]="form.invalid">Submit</button>
+		<form [formGroup]="form" (ngSubmit)="submit()" class="w-full max-w-xs">
+			<hlm-field-group>
+				<hlm-field>
+					<label hlmFieldLabel for="dateRange">Enter a date range</label>
+					<hlm-date-range-picker
+						buttonId="dateRange"
+						[min]="minDate"
+						[max]="maxDate"
+						formControlName="range"
+						[autoCloseOnEndSelection]="true"
+					>
+						<span>Enter a date range</span>
+					</hlm-date-range-picker>
+				</hlm-field>
+				<hlm-field orientation="horizontal">
+					<button type="submit" hlmBtn [disabled]="form.invalid">Submit</button>
+				</hlm-field>
+			</hlm-field-group>
 		</form>
 	`,
 })

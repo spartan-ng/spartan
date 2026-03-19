8f801b1b51a5541ae570db649e2445207bb4d9b5
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-03-19 16:52:05 +0100

    feat(combobox): open popup with arrow down/up

--- libs/brain/combobox/src/lib/brn-combobox-trigger.ts
+++ libs/brain/combobox/src/lib/brn-combobox-trigger.ts
@@ -7,10 +7,18 @@ import { injectBrnComboboxBase } from './brn-combobox.token';
 		type: 'button',
 		'[attr.aria-disabled]': '_disabled() ? "true" : null',
 		'[attr.disabled]': '_disabled() ? "" : null',
+		'(keydown)': 'onKeyDown($event)',
 	},
 })
 export class BrnComboboxTrigger<T> {
 	private readonly _combobox = injectBrnComboboxBase<T>();
 
 	protected readonly _disabled = this._combobox.disabledState;
+
+	/** Listen for keydown events */
+	protected onKeyDown(event: KeyboardEvent): void {
+		if (!this._combobox.isExpanded() && (event.key === 'ArrowDown' || event.key === 'ArrowUp')) {
+			this._combobox.open();
+		}
+	}
 }

eae788452d543a8c17fef73084df8f6c5b525895
Toni Škobić <toni.skobic@gmail.com>
2026-03-27 11:27:12 +0100

    fix: remove aria invalid and fix chips focus styles

--- libs/helm/combobox/src/lib/hlm-combobox-chips.ts
+++ libs/helm/combobox/src/lib/hlm-combobox-chips.ts
@@ -1,19 +1,34 @@
-import { Directive } from '@angular/core';
-import { BrnComboboxAnchor, BrnComboboxInputWrapper, BrnComboboxPopoverTrigger } from '@spartan-ng/brain/combobox';
+import { computed, Directive } from '@angular/core';
+import {
+	BrnComboboxAnchor,
+	BrnComboboxInputWrapper,
+	BrnComboboxPopoverTrigger,
+	injectBrnComboboxBase,
+} from '@spartan-ng/brain/combobox';
 import { classes } from '@spartan-ng/helm/utils';
 
 @Directive({
 	selector: '[hlmComboboxChips],hlm-combobox-chips',
 	hostDirectives: [BrnComboboxInputWrapper, BrnComboboxAnchor, BrnComboboxPopoverTrigger],
 	host: {
-		'data-slott': 'combobox-chips',
+		'data-slot': 'combobox-chips',
 	},
 })
 export class HlmComboboxChips {
+	private readonly _combobox = injectBrnComboboxBase();
+
+	protected readonly _spartanInvalid = computed(() => this._combobox.controlState?.()?.spartanInvalid);
+
+	protected readonly _errorStateClass = computed(() =>
+		this._spartanInvalid?.()
+			? 'border-destructive focus-within:border-destructive focus-within:ring-destructive/20 dark:focus-within:ring-destructive/40'
+			: '',
+	);
+
 	constructor() {
-		classes(
-			() =>
-				'dark:bg-input/30 border-input focus-within:border-ring focus-within:ring-ring/50 has-[>[data-matches-spartan-invalid=true]]:ring-destructive/20 dark:has-[>[data-matches-spartan-invalid=true]]:ring-destructive/40 has-[>[data-matches-spartan-invalid=true]]:border-destructive dark:has-[>[data-matches-spartan-invalid=true]]:border-destructive/50 has-data-[slot=combobox-chip]:px-1.5; flex min-h-9 flex-wrap items-center gap-1.5 rounded-md border bg-transparent bg-clip-padding px-2.5 py-1.5 text-sm shadow-xs transition-[color,box-shadow] focus-within:ring-[3px] has-[>[data-matches-spartan-invalid=true]]:ring-[3px]',
-		);
+		classes(() => [
+			'dark:bg-input/30 border-input focus-within:border-ring focus-within:ring-ring/50 has-data-[slot=combobox-chip]:px-1.5; flex min-h-9 flex-wrap items-center gap-1.5 rounded-md border bg-transparent bg-clip-padding px-2.5 py-1.5 text-sm shadow-xs transition-[color,box-shadow] focus-within:ring-[3px]',
+			this._errorStateClass(),
+		]);
 	}
 }

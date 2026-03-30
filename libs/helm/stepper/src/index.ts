456ac8710a3555d029e79b4ae5c7934c091febbe
oussema sahbeni <oussemasahbeni300@gmail.com>
2026-03-30 22:31:12 +0100

    feat(stepper): refactor stepper button components into separate files

--- libs/helm/stepper/src/index.ts
+++ libs/helm/stepper/src/index.ts
@@ -3,14 +3,16 @@ import { HlmStepContent } from './lib/hlm-step-content';
 import { HlmStepHeader } from './lib/hlm-step-header';
 import { HlmStepLabel } from './lib/hlm-step-label';
 import { HlmStepper } from './lib/hlm-stepper';
-import { HlmStepperNext, HlmStepperPrevious } from './lib/hlm-stepper-button';
+import { HlmStepperNext } from './lib/hlm-stepper-button-next';
+import { HlmStepperPrevious } from './lib/hlm-stepper-button-previous';
 
 export * from './lib/hlm-step';
 export * from './lib/hlm-step-content';
 export * from './lib/hlm-step-header';
 export * from './lib/hlm-step-label';
 export * from './lib/hlm-stepper';
-export * from './lib/hlm-stepper-button';
+export * from './lib/hlm-stepper-button-next';
+export * from './lib/hlm-stepper-button-previous';
 export * from './lib/stepper.token';
 
 export const HlmStepperImports = [

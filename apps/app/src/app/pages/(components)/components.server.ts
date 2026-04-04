40bd17f6ed937b27851da9143a2a52c8c32a9723
goetzrobin <robin@thejournai.com>
2026-04-04 13:19:52 +0200

    fix: fix types and storage parsing for prod build

--- apps/app/src/app/pages/(components)/components.server.ts
+++ apps/app/src/app/pages/(components)/components.server.ts
@@ -8,12 +8,9 @@ type ManualInstallSnippets = Record<string, Record<Style, string>>;
 export const load = async () => {
 	const storage = useStorage('assets:data');
 	const [docsData, primitivesData, manualInstallSnippets] = await Promise.all([
-		storage.getItem<ComponentApiData>('ui-api.json'),
-		storage.getItem<PrimitiveSnippets>('primitives-snippets.json'),
-		storage.getItem<ManualInstallSnippets>('manual-install-snippets.json'),
+		storage.getItem('ui-api.json').then((data) => JSON.parse(data as string) as ComponentApiData),
+		storage.getItem('primitives-snippets.json').then((data) => JSON.parse(data as string) as PrimitiveSnippets),
+		storage.getItem('manual-install-snippets.json').then((data) => JSON.parse(data as string) as ManualInstallSnippets),
 	]);
-	if (!docsData || !primitivesData || !manualInstallSnippets) {
-		throw new Error('Failed to load required data files');
-	}
 	return { docsData, primitivesData, manualInstallSnippets };
 };

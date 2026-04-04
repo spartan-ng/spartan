f996ed7468dfcb7a43007ab3e45477f2c13d3963
goetzrobin <robin@thejournai.com>
2026-04-04 23:54:13 +0200

    fix: revert nitropack and useStorage and use $fetch instead
    useStorage was the wrong tool.
    they are available in our public assets.
    they are not bundled in our js files.
    we load our json files during ssr with a fetch call.
    this avoids bundler running out of ram.
    this still lets us use their data during pre-rendering / ssr

--- apps/app/src/app/pages/(components)/components.server.ts
+++ apps/app/src/app/pages/(components)/components.server.ts
@@ -1,16 +1,12 @@
 import type { Style } from '@spartan-ng/registry';
-import { useStorage } from 'nitropack/runtime';
 import type { PrimitiveSnippets } from '../../core/models/primitives-snippets.model';
 import type { ComponentApiData } from '../../core/models/ui-docs.model';
 
-type ManualInstallSnippets = Record<string, Record<Style, string>>;
-
 export const load = async () => {
-	const storage = useStorage('assets:data');
 	const [docsData, primitivesData, manualInstallSnippets] = await Promise.all([
-		storage.getItem('ui-api.json').then((data) => JSON.parse(data as string) as ComponentApiData),
-		storage.getItem('primitives-snippets.json').then((data) => JSON.parse(data as string) as PrimitiveSnippets),
-		storage.getItem('manual-install-snippets.json').then((data) => JSON.parse(data as string) as ManualInstallSnippets),
+		$fetch<ComponentApiData>('/data/ui-api.json'),
+		$fetch<PrimitiveSnippets>('/data/primitives-snippets.json'),
+		$fetch<Record<string, Record<Style, string>>>('/data/manual-install-snippets.json'),
 	]);
 	return { docsData, primitivesData, manualInstallSnippets };
 };

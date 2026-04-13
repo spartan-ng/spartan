7ce0eff6dbe08bb39636a6ed832a3e8455ceb90b
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-04-13 13:01:29 +0200

    docs: cleanup key

--- apps/app/src/app/pages/(components)/components/(skeleton)/skeleton--table.preview.ts
+++ apps/app/src/app/pages/(components)/components/(skeleton)/skeleton--table.preview.ts
@@ -12,7 +12,7 @@ import { HlmSkeletonImports } from '@spartan-ng/helm/skeleton';
 	template: `
 		<div class="flex w-full max-w-sm flex-col gap-2">
 			@for (row of _rows; track $index) {
-				<div class="flex gap-4" key="{index}">
+				<div class="flex gap-4">
 					<hlm-skeleton class="h-4 flex-1" />
 					<hlm-skeleton class="h-4 w-24" />
 					<hlm-skeleton class="h-4 w-20" />

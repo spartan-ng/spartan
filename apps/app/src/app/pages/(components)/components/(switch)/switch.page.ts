22917aa4361798e4da3d07857baddd9a16c852b0
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2026-06-08 23:53:26 +0100

    feat(switch): add rtl example
    Add a switch--rtl.example.ts preview that toggles dir via the host binding and
    TranslateService (en/ar/he), mirroring the alert RTL example. Wire it into the
    docs page with spartan-header-rtl and a spartanRtlCodePreview tab block.

--- apps/app/src/app/pages/(components)/components/(switch)/switch.page.ts
+++ apps/app/src/app/pages/(components)/components/(switch)/switch.page.ts
@@ -1,6 +1,9 @@
 import type { RouteMeta } from '@analogjs/router';
 import { Component, computed, inject } from '@angular/core';
 import { PrimitiveSnippetsService } from '@spartan-ng/app/app/core/services/primitive-snippets.service';
+import { SwitchRtlPreview } from '@spartan-ng/app/app/pages/(components)/components/(switch)/switch--rtl.example';
+import { CodeRtlPreview } from '@spartan-ng/app/app/shared/code/code-rtl-preview';
+import { RtlHeader } from '@spartan-ng/app/app/shared/code/rtl-header';
 import { InstallTabs } from '@spartan-ng/app/app/shared/layout/install-tabs';
 import { Code } from '../../../../shared/code/code';
 import { CodePreview } from '../../../../shared/code/code-preview';
@@ -36,6 +39,9 @@ export const routeMeta: RouteMeta = {
 		PageBottomNav,
 		PageBottomNavLink,
 		SwitchPreview,
+		RtlHeader,
+		CodeRtlPreview,
+		SwitchRtlPreview,
 	],
 	template: `
 		<section spartanMainSection>
@@ -60,6 +66,14 @@ export const routeMeta: RouteMeta = {
 				<spartan-code [code]="_defaultSkeleton" />
 			</div>
 
+			<spartan-header-rtl />
+			<spartan-tabs firstTab="Preview" secondTab="Code">
+				<div spartanRtlCodePreview firstTab>
+					<spartan-switch-rtl-preview />
+				</div>
+				<spartan-code secondTab [code]="_rtlCode()" />
+			</spartan-tabs>
+
 			<spartan-section-sub-heading id="brn-api">Brain API</spartan-section-sub-heading>
 			<spartan-ui-api-docs docType="brain" />
 
@@ -77,6 +91,7 @@ export const routeMeta: RouteMeta = {
 export default class SkeletonPage {
 	private readonly _snippets = inject(PrimitiveSnippetsService).getSnippets('switch');
 	protected readonly _defaultCode = computed(() => this._snippets()['default']);
+	protected readonly _rtlCode = computed(() => this._snippets()['rtl']);
 	protected readonly _defaultSkeleton = defaultSkeleton;
 	protected readonly _defaultImports = defaultImports;
 }

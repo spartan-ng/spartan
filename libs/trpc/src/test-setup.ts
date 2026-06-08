d5846124227906f7e20956b00129e31fc0cfc1f0
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2026-06-08 19:42:28 +0100

    fix(trpc): drop invalid environment providers from the test env module
    The Nx 22 migration wrapped provideZoneChangeDetection() in an @NgModule,
    but EnvironmentProviders are not valid in @NgModule providers and Angular
    21 throws NG0207 at runtime. Restore the plain initTestEnvironment setup
    (matching the docs app); the explicit zone provider is unnecessary.

--- libs/trpc/src/test-setup.ts
+++ libs/trpc/src/test-setup.ts
@@ -1,6 +1,5 @@
 import '@analogjs/vite-plugin-angular/setup-vitest';
 import '@angular/compiler';
-import { NgModule, provideZoneChangeDetection } from '@angular/core';
 
 /**
  * Initialize TestBed for all tests inside of router
@@ -8,7 +7,4 @@ import { NgModule, provideZoneChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-@NgModule({ providers: [provideZoneChangeDetection()] })
-export class ZoneChangeDetectionModule {}
-
-TestBed.initTestEnvironment([ZoneChangeDetectionModule, BrowserDynamicTestingModule], platformBrowserDynamicTesting());
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());

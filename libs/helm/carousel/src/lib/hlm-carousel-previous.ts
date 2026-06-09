4a511734bd5636419a70add121133a88c8ff80c4
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2026-06-09 19:57:08 +0100

    feat(carousel): add style support and rtl example
    Closes #1396

--- libs/helm/carousel/src/lib/hlm-carousel-previous.ts
+++ libs/helm/carousel/src/lib/hlm-carousel-previous.ts
@@ -18,7 +18,7 @@ import { HlmCarousel } from './hlm-carousel';
 		'(click)': '_carousel.scrollPrev()',
 	},
 	template: `
-		<ng-icon hlm size="sm" name="lucideArrowLeft" />
+		<ng-icon hlm size="sm" name="lucideArrowLeft" class="rtl:rotate-180" />
 		<span class="sr-only">Previous slide</span>
 	`,
 })
@@ -29,9 +29,9 @@ export class HlmCarouselPrevious {
 
 	private readonly _computedClass = computed(() =>
 		hlm(
-			'absolute h-8 w-8 rounded-full',
+			'spartan-carousel-previous absolute h-8 w-8',
 			this._carousel.orientation() === 'horizontal'
-				? 'top-1/2 -left-12 -translate-y-1/2'
+				? '-start-12 top-1/2 -translate-y-1/2'
 				: '-top-12 left-1/2 -translate-x-1/2 rotate-90',
 		),
 	);

88ee7112dc723cf8ed8d69de0960db4c817e9aaf
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-06-18 17:30:42 +0200

    fix(date-picker): arabic first day of week to keep the same start date

--- apps/app/src/app/pages/(components)/components/(date-picker)/date-picker--rtl.preview.ts
+++ apps/app/src/app/pages/(components)/components/(date-picker)/date-picker--rtl.preview.ts
@@ -49,7 +49,7 @@ export const CALENDAR_I18N: Record<'en' | 'ar' | 'he', BrnCalendarI18n> = {
 		labelPrevious: () => 'الشهر السابق',
 		labelNext: () => 'الشهر التالي',
 		labelWeekday: (i) => ['الأحد', 'الإثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت'][i],
-		firstDayOfWeek: () => 6,
+		firstDayOfWeek: () => 0,
 	},
 	he: {
 		formatWeekdayName: (i) => ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ש'][i],

71b5ee8ebacfa25f62fae6db6a51855052c098d8
mehrabix <mehrabi@post.com>
2026-06-24 11:32:41 +0330

    fix(card): add hlm-card-content element selector to match element usage

--- libs/helm/card/src/lib/hlm-card-content.ts
+++ libs/helm/card/src/lib/hlm-card-content.ts
@@ -2,7 +2,7 @@ import { Directive } from '@angular/core';
 import { classes } from '@spartan-ng/helm/utils';
 
 @Directive({
-	selector: '[hlmCardContent]',
+	selector: '[hlmCardContent],hlm-card-content',
 	host: { 'data-slot': 'card-content' },
 })
 export class HlmCardContent {

c6731a41e73462a375c1a095f3f1a04abcdef74c
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-09-18 13:35:33 +0200

    docs(switch): add more examples

--- apps/app/src/app/pages/(components)/components/(switch)/switch--sizes.example.ts
+++ apps/app/src/app/pages/(components)/components/(switch)/switch--sizes.example.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
-import { HlmLabel } from '@spartan-ng/helm/label';
+import { HlmFieldImports } from '@spartan-ng/helm/field';
 import { HlmSwitch } from '@spartan-ng/helm/switch';
 
 @Component({
 	selector: 'spartan-switch-sizes',
-	imports: [HlmLabel, HlmSwitch],
-	host: {
-		class: 'flex items-center gap-6',
-	},
+	imports: [HlmFieldImports, HlmSwitch],
+	host: { class: 'w-full max-w-40' },
 	template: `
-		<label class="flex items-center" hlmLabel>
-			<hlm-switch class="me-2" size="sm" />
-			Small
-		</label>
-		<label class="flex items-center" hlmLabel>
-			<hlm-switch class="me-2" size="default" />
-			Default
-		</label>
+		<hlm-field-group>
+			<hlm-field orientation="horizontal">
+				<hlm-switch inputId="switch-size-sm" size="sm" />
+				<label hlmFieldLabel for="switch-size-sm">Small</label>
+			</hlm-field>
+			<hlm-field orientation="horizontal">
+				<hlm-switch inputId="switch-size-default" />
+				<label hlmFieldLabel for="switch-size-default">Default</label>
+			</hlm-field>
+		</hlm-field-group>
 	`,
 })
 export class SwitchSizes {}

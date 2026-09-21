fcacf0d14881a64960d05d4234b6228adc78ee3e
bsarnii <69685787+bsarnii@users.noreply.github.com>
2026-09-21 11:51:06 +0200

    feat(sonner): add data-testid attribute for e2e testing (#1761)
    Co-authored-by: arnold.schiopu <arnold.schiopu@converia.de>

--- libs/brain/sonner/src/lib/brn-toast.ts
+++ libs/brain/sonner/src/lib/brn-toast.ts
@@ -50,6 +50,7 @@ import type { ToastAction, ToastProps, ToastT } from './types';
 			[attr.data-invert]="_invert()"
 			[attr.data-swipe-out]="_swipeOut()"
 			[attr.data-expanded]="expanded() || (expandByDefault() && _mounted())"
+			[attr.data-testid]="toast().testId"
 			[style]="_toastStyle()"
 			(pointerdown)="onPointerDown($event)"
 			(pointerup)="onPointerUp()"
